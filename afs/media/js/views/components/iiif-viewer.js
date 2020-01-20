e6cdfa9cfd04de9c2ad518ea987eb6ca01248003
Rob Gaston <rgaston@fargeo.com>
2020-01-20 13:44:08 -0800

    further scaffolds iiif component, re: #4

--- afs/media/js/views/components/iiif-viewer.js
+++ afs/media/js/views/components/iiif-viewer.js
@@ -3,7 +3,15 @@ define([
     'views/components/workbench'
 ], function(ko, WorkbenchViewmodel) {
     var IIIFViewerViewmodel = function(params) {
+        var self = this;
+        
         WorkbenchViewmodel.apply(this, [params]);
+        
+        this.showGallery = ko.observable(false);
+        
+        this.toggleGallery = function() {
+            self.showGallery(!self.showGallery());
+        };
     };
     ko.components.register('iiif-viewer', {
         viewModel: IIIFViewerViewmodel,
