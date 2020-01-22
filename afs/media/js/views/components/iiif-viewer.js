f61fc1253ae12e486ba3170889c3fb7f303bdc09
Rob Gaston <rgaston@fargeo.com>
2020-01-22 10:21:16 -0800

    further scaffolds iiif component re: #4

--- afs/media/js/views/components/iiif-viewer.js
+++ afs/media/js/views/components/iiif-viewer.js
@@ -1,9 +1,35 @@
 define([
     'knockout',
-    'views/components/workbench'
-], function(ko, WorkbenchViewmodel) {
+    'leaflet',
+    'views/components/workbench',
+    'leaflet-iiif',
+    'bindings/leaflet'
+], function(ko, L, WorkbenchViewmodel) {
     var IIIFViewerViewmodel = function(params) {
         var self = this;
+        
+        this.map = ko.observable();
+        this.manifest = ko.observable(params.manifest);
+        this.canvas = ko.observable(params.canvas);
+        this.manifestData = ko.observable();
+        this.sequences = ko.pureComputed(function() {
+            var manifestData = self.manifestData();
+            return manifestData ? manifestData.sequences : [];
+        });
+        
+        var getManifestData = function(manifestURL) {
+            if (manifestURL) {
+                fetch(manifestURL)
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(manifestData) {
+                        self.manifestData(manifestData);
+                    });
+            }
+        };
+        this.manifest.subscribe(getManifestData);
+        getManifestData(params.manifest);
         
         WorkbenchViewmodel.apply(this, [params]);
         
@@ -12,6 +38,43 @@ define([
         this.toggleGallery = function() {
             self.showGallery(!self.showGallery());
         };
+        
+        this.leafletConfig = {
+            center: [0, 0],
+            crs: L.CRS.Simple,
+            zoom: 0,
+            afterRender: this.map
+        };
+
+        var canvasLayer;
+        var addCanvasLayer = function() {
+            var map = self.map();
+            var canvas = self.canvas();
+            if (map && canvas) {
+                if (canvasLayer) {
+                    map.removeLayer(canvasLayer);
+                    canvasLayer = undefined;
+                }
+                if (canvas) {
+                    canvasLayer = L.tileLayer.iiif(
+                        canvas + '/info.json'
+                    ).addTo(map);
+                }
+            }
+        };
+        this.map.subscribe(addCanvasLayer);
+        this.canvas.subscribe(addCanvasLayer);
+        this.manifestData.subscribe(function(manifestData) {
+            if (!self.canvas() && manifestData.sequences.length > 0) {
+                var sequence = manifestData.sequences[0];
+                if (sequence.canvases.length > 0) {
+                    var canvas = sequence.canvases[0];
+                    if (canvas.images.length > 0) {
+                        self.canvas(canvas.images[0].resource.service['@id']);
+                    }
+                }
+            }
+        });
     };
     ko.components.register('iiif-viewer', {
         viewModel: IIIFViewerViewmodel,
