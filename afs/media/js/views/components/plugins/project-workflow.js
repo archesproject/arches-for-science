f3f0ed8652db50cc7625ea1c054c9b61ae202d45
Galen Mancino <whatisgalen@gmail.com>
2020-05-01 15:47:45 -0700

    set appropriate nodeids, graphid, & general labels, re #111

--- afs/media/js/views/components/plugins/project-workflow.js
+++ afs/media/js/views/components/plugins/project-workflow.js
@@ -12,28 +12,27 @@ define([
 
             params.steps = [
                 {
-                    title: 'Project Details',
-                    name: 'setprojectdetails',
+                    title: 'Activity Name',
+                    name: 'setactivityname',
                     description: 'Identify the project and its objectives',
                     component: 'views/components/workflows/new-tile-step',
                     componentname: 'new-tile-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4ba-a250-11e9-9b20-00224800b26d',
+                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                    nodegroupid: '0b926359-ca85-11e9-ac9c-a4d18cec433a',
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
                     required: true,
                     icon: 'fa-code-fork',
-                    wastebin: {resourceid: null, description: 'a consulation instance'}
+                    wastebin: {resourceid: null, description: 'an activity instance'}
                 },
                 {
-                    title: 'Consultation GeoJSON',
-                    name: 'consultationlocation',
-                    description: 'Set geospatial data for this consultation',
-                    component: 'views/components/workflows/consultation-map-step',
-                    componentname: 'consultation-map-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4c6-a250-11e9-a54d-00224800b26d',
+                    title: 'Activity Statement',
+                    name: 'setactivitystatement',
+                    description: 'Set the Activity Statement',
+                    componentname: 'new-tile-step',
+                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                    nodegroupid: '0b92a414-ca85-11e9-b725-a4d18cec433a',
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
@@ -41,81 +40,40 @@ define([
                     icon: 'fa-map-marker'
                 },
                 {
-                    title: 'Consultation Dates',
-                    name: 'setdatedetails',
+                    title: 'Activity Timespan',
+                    name: 'setactivitytimespan',
                     description: 'Consultation Dates',
-                    component: 'views/components/workflows/consultation-dates-step',
-                    componentname: 'consultation-dates-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-002s24800b26d',
-                    nodegroupid: '8d41e4a5-a250-11e9-840c-00224800b26d',
+                    component: 'views/components/workflows/new-tile-step',
+                    componentname: 'new-tile-step',
+                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                    nodegroupid: '0b925e3a-ca85-11e9-a308-a4d18cec433a',
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
                     required: true,
-                    class: 'hide-completion-date',
                     icon: 'fa-calendar-o'
                 },
                 {
-                    title: 'Consultation Details',
-                    name: 'setconsdetails',
+                    title: 'Project Team',
+                    name: 'setprojectteam',
                     description: 'Consultation Details',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4c0-a250-11e9-a7e3-00224800b26d',
+                    component: 'views/components/workflows/new-multi-tile-step',
+                    componentname: 'new-multi-tile-step',
+                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                    nodegroupid: '0b92abdc-ca85-11e9-8a23-a4d18cec433a',
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
-                    required: true,
+                    required: false,
                     icon: 'fa-list-alt'
                 },
                 {
-                    title: 'Reference Numbers',
-                    name: 'setrefnumbers',
-                    description: 'Application Reference Numbers',
-                    component: 'views/components/workflows/new-multi-tile-step',
-                    componentname: 'new-multi-tile-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4a2-a250-11e9-82f1-00224800b26d',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: false,
-                    icon: 'fa-hashtag'
-                },
-                {
-                    title: 'Application Proposal',
-                    description: 'Summary of the Application that will be reviewed under this Consultation',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4bd-a250-11e9-89e8-00224800b26d',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: false,
-                    icon: 'fa-clipboard'
-                },
-                {
-                    title: 'Contacts',
-                    description: 'Identify the key people/organizations associated with this consultation',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '8d41e4a8-a250-11e9-aff0-00224800b26d',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: false,
-                    icon: 'fa-users'
-                },
-                {
-                    title: 'Add Consulation Complete',
+                    title: 'Add Project Complete',
                     description: 'Choose an option below',
-                    component: 'views/components/workflows/consultations-final-step',
-                    componentname: 'consultations-final-step',
-                    graphid: '8d41e49e-a250-11e9-9eab-00224800b26d',
-                    nodegroupid: '6a773228-db20-11e9-b6dd-784f435179ea', // consultation status
+                    component: 'views/components/workflows/afs-final-step',
+                    componentname: 'afs-final-step',
+                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                    nodegroupid: '',
                     icon: 'fa-check',
                     resourceid: null,
                     tileid: null,
@@ -124,14 +82,13 @@ define([
             ];
 
             Workflow.apply(this, [params]);
-            this.quitUrl = "/arches-her" + arches.urls.plugin('init-workflow');
-            console.log(this.quitUrl);
-            self.getJSON('consultation-workflow');
+            this.quitUrl = arches.urls.plugin('init-workflow');
+            self.getJSON('project-workflow');
 
             self.activeStep.subscribe(this.updateState);
 
             self.ready(true);
         },
-        template: { require: 'text!templates/views/components/plugins/consultation-workflow.htm' }
+        template: { require: 'text!templates/views/components/plugins/project-workflow.htm' }
     });
 });
