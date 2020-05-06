11a5e2dad6fa7affd3cdd865401e02db52a5de25
Galen Mancino <whatisgalen@gmail.com>
2020-05-06 16:47:47 -0700

    adds physical_thing_search_terms view, also removes slash in front of url pattern strings, re #116

--- afs/urls.py
+++ afs/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from arches.app.views.plugin import PluginView
+from afs.views.physical_thing_search import PhysicalThingSearchView
 
 urlpatterns = [
     url(r"^", include("arches.urls")),
-    url(r"^/plugins/sampling-activity-workflow", PluginView.as_view(), name="sampling-activity-workflow"),
-    url(r"^/plugins/project-workflow", PluginView.as_view(), name="project-workflow"),
-    url(r"^/plugins/init-workflow", PluginView.as_view(), name="init-workflow"),
+    url(r"^plugins/sampling-activity-workflow", PluginView.as_view(), name="sampling-activity-workflow"),
+    url(r"^plugins/project-workflow", PluginView.as_view(), name="project-workflow"),
+    url(r"^plugins/init-workflow", PluginView.as_view(), name="init-workflow"),
+    url(r'^physical_thing_search_terms', PhysicalThingSearchView.as_view(), name="physical_thing_search_results"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
