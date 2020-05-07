d6feb9e64996cbe0f2ac04a30374a5bf89ff13de
Cyrus Hiatt <chiatt@fargeo.com>
2020-05-07 16:00:01 -0700

    Updates url name and adds url to javascript.htm re #112

--- afs/urls.py
+++ afs/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r"^plugins/sampling-activity-workflow", PluginView.as_view(), name="sampling-activity-workflow"),
     url(r"^plugins/research-activity-workflow", PluginView.as_view(), name="research-activity-workflow"),
     url(r"^plugins/init-workflow", PluginView.as_view(), name="init-workflow"),
-    url(r"^physical_thing_search_terms", PhysicalThingSearchView.as_view(), name="physical_thing_search_results"),
+    url(r"^physical-thing-search-results", PhysicalThingSearchView.as_view(), name="physical-thing-search-results"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
