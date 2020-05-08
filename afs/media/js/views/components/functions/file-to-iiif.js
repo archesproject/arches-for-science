a22fb24d379de5602254d2ba6ee38776d2e7bc21
Galen Mancino <whatisgalen@gmail.com>
2020-05-08 14:08:26 -0700

    fixes linting errors, re #122

--- afs/media/js/views/components/functions/file-to-iiif.js
+++ afs/media/js/views/components/functions/file-to-iiif.js
@@ -1,23 +1,25 @@
-define(['knockout',
-        'knockout-mapping',
-        'views/list',
-        'viewmodels/function',
-        'bindings/chosen'],
-function (ko, koMapping, ListView, FunctionViewModel, chosen) {
+define([
+    'jquery',
+    'underscore',
+    'knockout',
+    'knockout-mapping',
+    'views/list',
+    'viewmodels/function',
+    'bindings/chosen'],
+function($, _, ko, koMapping, ListView, FunctionViewModel, chosen) {
     return ko.components.register('views/components/functions/file-to-iiif', {
         viewModel: function(params) {
             FunctionViewModel.apply(this, arguments);
-            var self = this;
             var nodegroups = {};
             this.cards = ko.observableArray();
-            this.selected_nodegroup = params.config.selected_nodegroup
-            this.selected_nodegroup.subscribe(function(ng){
-              console.log('selected nodegroup id:', ng);
-            })
+            this.selectedNodegroup = params.config.selected_nodegroup;
+            this.selectedNodegroup.subscribe(function(ng){
+                console.log('selected nodegroup id:', ng);
+            });
 
             this.graph.cards.forEach(function(card){
                 var found = !!_.find(this.graph.nodegroups, function(nodegroup){
-                    return nodegroup.parentnodegroup_id === card.nodegroup_id
+                    return nodegroup.parentnodegroup_id === card.nodegroup_id;
                 }, this);
                 if(!found && !(card.nodegroup_id in nodegroups)){
                     this.cards.push(card);
@@ -31,4 +33,4 @@ function (ko, koMapping, ListView, FunctionViewModel, chosen) {
             require: 'text!templates/views/components/functions/file-to-iiif.htm'
         }
     });
-})
+});
