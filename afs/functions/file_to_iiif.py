b4375240a76e19435a067c6a4c33c762cedd8b2b
Galen Mancino <whatisgalen@gmail.com>
2020-05-12 10:45:23 -0700

    adds cantaloupe_http_endpoint, re #122

--- afs/functions/file_to_iiif.py
+++ afs/functions/file_to_iiif.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import shutil
-from afs.settings import CANTALOUPE_DIR, MEDIA_ROOT
+from afs.settings import CANTALOUPE_DIR, CANTALOUPE_HTTP_ENDPOINT, MEDIA_ROOT
 from arches.app.functions.base import BaseFunction
 from arches.app.models import models
 
