6cd17f5a84e200854dbbe0ff552fb699b7af2e7d
stickler-ci <support@stickler-ci.com>
2020-05-20 15:59:00 +0000

    Fixing style errors.

--- afs/views/physical_things_in_set.py
+++ afs/views/physical_things_in_set.py
@@ -7,7 +7,9 @@ from arches.app.models.resource import Resource
 class PhysicalThingSetView(View):
     def get(self, request):
         # via an id for a set, returns list of phys things and stuff necessary
-        set_resourceid = None if request.GET.get('resourceid') == '' or request.GET.get('resourceid') == None else (request.GET.get('resourceid'))
+        set_resourceid = (
+            None if request.GET.get("resourceid") == "" or request.GET.get("resourceid") == None else (request.GET.get("resourceid"))
+        )
         # set_resourceid = 'cc221153-08a1-49ba-bc07-35fc3b8d783e'
         print(set_resourceid)
         related_things = models.ResourceXResource.objects.filter(resourceinstanceidfrom=set_resourceid)
@@ -24,12 +26,12 @@ class PhysicalThingSetView(View):
             for tile in resource.tiles:
                 for nodeid in list(tile.data.keys()):
                     node = models.Node.objects.get(nodeid=nodeid)
-                    if node.datatype == 'annotation' and tile.data[str(node.nodeid)]['features']:
+                    if node.datatype == "annotation" and tile.data[str(node.nodeid)]["features"]:
                         # if tile.data[str(node.nodeid)]['features']:
-                            # iiif = tile.data
+                        # iiif = tile.data
                         iiif_tile_dict = {
-                            "thumbnail": tile.data[str(node.nodeid)]['features'][0]['properties']['canvas'] + '/full/!75,75/0/default.jpg',
-                            "tileid": tile.tileid
+                            "thumbnail": tile.data[str(node.nodeid)]["features"][0]["properties"]["canvas"] + "/full/!75,75/0/default.jpg",
+                            "tileid": tile.tileid,
                         }
                         iiif.append(iiif_tile_dict)
             new_dict = {
