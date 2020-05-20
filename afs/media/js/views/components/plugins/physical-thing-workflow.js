1680aa804b3dfb5cebb6160ac29457ced520e55d
Galen Mancino <whatisgalen@gmail.com>
2020-05-20 08:52:16 -0700

    adds placeholder for physical-thing-workflow, re #147

---
+++ afs/media/js/views/components/plugins/physical-thing-workflow.js
@@ -0,0 +1,110 @@
+define([
+    'knockout',
+    'jquery',
+    'arches',
+    'viewmodels/workflow',
+    'viewmodels/workflow-step'
+], function(ko, $, arches, Workflow) {
+    return ko.components.register('physical-thing-workflow', {
+        viewModel: function(params) {
+
+            var self = this;
+            // make a physical thing, then ref a visual work, then digital resource
+
+            params.steps = [
+                // {
+                //     title: 'Activity Name',
+                //     name: 'setactivityname',
+                //     description: 'Identify the project and its objectives',
+                //     component: 'views/components/workflows/new-tile-step',
+                //     componentname: 'new-tile-step',
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: '0b926359-ca85-11e9-ac9c-a4d18cec433a',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null,
+                //     required: true,
+                //     icon: 'fa-code-fork',
+                //     wastebin: {resourceid: null, description: 'an activity instance'}
+                // },
+                // {
+                //     title: 'Activity Statement',
+                //     name: 'setactivitystatement',
+                //     description: 'Set the Activity Statement',
+                //     component: 'views/components/workflows/new-tile-step',
+                //     componentname: 'new-tile-step',
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: '0b92a414-ca85-11e9-b725-a4d18cec433a',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null,
+                //     required: false,
+                //     icon: 'fa-map-marker'
+                // },
+                // {
+                //     title: 'Activity Timespan',
+                //     name: 'setactivitytimespan',
+                //     description: 'Consultation Dates',
+                //     component: 'views/components/workflows/new-tile-step',
+                //     componentname: 'new-tile-step',
+                //     hiddenNodes: ['0b92f57d-ca85-11e9-a353-a4d18cec433a', '0b931623-ca85-11e9-b235-a4d18cec433a', '0b930905-ca85-11e9-8aca-a4d18cec433a'],
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: '0b925e3a-ca85-11e9-a308-a4d18cec433a',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null,
+                //     required: true,
+                //     icon: 'fa-calendar-o'
+                // },
+                // {
+                //     title: 'Project Team',
+                //     name: 'setprojectteam',
+                //     description: 'Consultation Details',
+                //     component: 'views/components/workflows/new-multi-tile-step',
+                //     componentname: 'new-multi-tile-step',
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: '0b92abdc-ca85-11e9-8a23-a4d18cec433a',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null,
+                //     required: false,
+                //     icon: 'fa-list-alt'
+                // },
+                // {
+                //     title: 'Physical Things',
+                //     name: 'createphysicalthingsset',
+                //     description: 'Create a set of physical things',
+                //     componentname: 'new-tile-step',
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: 'cc5d6df3-d477-11e9-9f59-a4d18cec433a',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null,
+                //     required: false,
+                //     icon: 'fa-archive'
+                // },
+                // {
+                //     title: 'Add Project Complete',
+                //     description: 'Choose an option below',
+                //     component: 'views/components/workflows/afs-final-step',
+                //     componentname: 'afs-final-step',
+                //     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
+                //     nodegroupid: '',
+                //     icon: 'fa-check',
+                //     resourceid: null,
+                //     tileid: null,
+                //     parenttileid: null
+                // }
+            ];
+
+            Workflow.apply(this, [params]);
+            this.quitUrl = arches.urls.plugin('init-workflow');
+            self.getJSON('physical-thing-workflow');
+
+            self.activeStep.subscribe(this.updateState);
+
+            self.ready(true);
+        },
+        template: { require: 'text!templates/views/components/plugins/physical-thing-workflow.htm' }
+    });
+});
