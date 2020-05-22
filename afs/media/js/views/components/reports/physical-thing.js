8e0dadc61a848175459c723e2890c701cd415203
Alexei Peters <apeters@fargeo.com>
2020-05-22 13:10:00 -0700

    finalize summary report template, re #115

--- afs/media/js/views/components/reports/physical-thing.js
+++ afs/media/js/views/components/reports/physical-thing.js
@@ -6,133 +6,129 @@ define(['underscore', 'knockout', 'arches', 'viewmodels/tabbed-report', 'utils/r
             TabbedReportViewModel.apply(this, [params]);
 
             if (params.summary) {
-                this.showTitleBar = params.showTitleBar || false;
-
-                this.getValue = function(queryClause, graph, tiles) {
-                    var nodeId;
-                    var foundTiles = [];
-                    var returnTiles = !!queryClause.returnTiles;
-
-                    var resolveWidgetLabel = function(cardWidgetPath, cards, widgets) {
-                        var cardName, cardids;
-                        var widgetLabel = '';
-                        var widgets = graph.widgets;
-                        var parts = cardWidgetPath.split('.');
-                        if (parts.length === 1) {
-                            widgetLabel = parts[0];
-                        } else if (parts.length === 2) {
-                            cardName = parts[0];
-                            widgetLabel = parts[1];
-                        }
-
-                        if (!!cardName) {
-                            cardids = cards.filter(function(card) {
-                                return card.name === cardName;
-                            }).map(function(card) {
-                                return card.cardid;
-                            });
-
-                            widgets = widgets.filter(function(widget) {
-                                return cardids.includes(widget.card_id);
-                            });
-                        }
-
-                        var nodeId = widgets.filter(function(widget) {
-                            return widget.label === widgetLabel;
-                        }).map(function(widget) {
-                            return widget.node_id;
-                        });
-
-                        return nodeId;
-                    }
-
-                    if (!!queryClause.nodeId) {
-                        nodeId = [queryClause.nodeId];
-                    } else if (!!queryClause.nodeName) {
-                        node = graph.nodes.find(function(node) {
-                            return node.name === queryClause.nodeName;
-                        });
-                        if (!!node) {
-                            nodeId = node.nodeid;
-                        }
-                    } else if (!!queryClause.widgetLabel) {
-                        nodeId = resolveWidgetLabel(queryClause.widgetLabel, graph.cards, graph.widgets);
-                    };
-
-                    if (!!nodeId && nodeId.length === 1) {
-                        nodeId = nodeId[0];
-                        foundTiles = tiles.filter(function(tile) {
-                            return Object.keys(tile.data).includes(nodeId);
-                        });
-                        if (!!queryClause.where) {
-                            if (!!queryClause.where.nodeId) {
-                                foundTiles = foundTiles.filter(function(tile) {
-                                    if (!!queryClause.where.contains) {
-                                        return tile.data[queryClause.where.nodeId].includes(queryClause.where.contains);
-                                    }
-                                    return false;
-                                });
-                            } else if (!!queryClause.where.widgetLabel) {
-                                var whereNodeId = resolveWidgetLabel(queryClause.where.widgetLabel, graph.cards, graph.widgets);
-                                if (!!whereNodeId && whereNodeId.length === 1) {
-                                    foundTiles = foundTiles.filter(function(tile) {
-                                        if (!!queryClause.where.contains) {
-                                            return tile.data[whereNodeId].includes(queryClause.where.contains);
-                                        }
-                                        return false;
-                                    });
-                                }
-                            }
-                        }
-
-                        if (returnTiles) {
-                            return foundTiles;
-                        } else {
-                            return foundTiles.map(function name(tile) {
-                                return tile.data[nodeId];
-                            })
-                        }
-                    }
-                    return undefined;
-                };
-
                 const Identifier_Content_nodeid = '22c169b5-b498-11e9-bdad-a4d18cec433a';
                 const Identifier_Type = '22c15cfa-b498-11e9-b5e3-a4d18cec433a';
                 const GallerySystemsTMSid = '26094e9c-2702-4963-adee-19ad118f0f5a';
-                this.gallerySystemsTMSid = this.getValue({
+                this.gallerySystemsTMSid = resourceUtils.getNodeValues({
                     nodeId: Identifier_Content_nodeid,
                     where: {
                         nodeId: Identifier_Type,
                         contains: GallerySystemsTMSid
                     },
                     returnTiles: false
-                }, this.report.graph, this.report.get('tiles'));
+                }, this.report.get('tiles'), this.report.graph);
 
+                this.artists = ko.observableArray();
+                this.artistIds = resourceUtils.getNodeValues({
+                    widgetLabel: 'Production (partitioned).carried out by',
+                    returnTiles: false
+                }, this.report.get('tiles'), this.report.graph);
+                this.artistIds.forEach(function(resourceid) {
+                    resourceUtils.lookupResourceInstanceData(resourceid)
+                        .then(function(data) {
+                            self.artists.push({ name: data._source.displayname, link: arches.urls.resource + '/' + resourceid });
+                        })
+                })
 
-                const DescriptionConceptValueId = 'df8e4cf6-9b0b-472f-8986-83d5b2ca28a0';
-                this.description = this.getValue({
+                const Description_Concept_valueid = 'df8e4cf6-9b0b-472f-8986-83d5b2ca28a0';
+                this.description = resourceUtils.getNodeValues({
                     widgetLabel: 'Statement about Thing.Text of Statement',
                     where: {
                         widgetLabel: 'Statement about Thing.Type of Statement',
-                        contains: DescriptionConceptValueId
+                        contains: Description_Concept_valueid
                     },
                     returnTiles: false
-                }, this.report.graph, this.report.get('tiles'));
+                }, this.report.get('tiles'), this.report.graph);
 
-                this.collectionSet = this.getValue({
-                    widgetLabel: 'In Collection or Set.member of',
-                    returnTiles: false
-                }, this.report.graph, this.report.get('tiles'));
-
-                console.log(this.collectionSet);
 
                 this.activities = ko.observableArray();
-                this.collectionSet.forEach(function(resourceid) {
+                var collectionSet = resourceUtils.getNodeValues({
+                    widgetLabel: 'In Collection or Set.member of',
+                    returnTiles: false
+                }, this.report.get('tiles'), this.report.graph);
+                collectionSet.forEach(function(resourceid) {
                     resourceUtils.lookupResourceInstanceData(resourceid)
                         .then(function(data) {
                             self.activities.push({ name: data._source.displayname, link: arches.urls.resource + '/' + resourceid });
                         })
                 })
+
+
+                var getLabel = function(object) {
+                    var label = object.label;
+                    if (Array.isArray(label)) {
+                        label = object.label[0]["@value"];
+                    }
+                    return label;
+                };
+
+                this.manifests = ko.observableArray();
+                var parseManifestJson = function(manifestData) {
+                    var sequences = manifestData ? manifestData.sequences : [];
+                    var canvases = [];
+                    sequences.forEach(function(sequence) {
+                        if (sequence.canvases) {
+                            sequence.label = getLabel(sequence);
+                            sequence.canvases.forEach(function(canvas) {
+                                canvas.label = getLabel(canvas);
+                                if (typeof canvas.thumbnail === 'object')
+                                    canvas.thumbnail = canvas.thumbnail["@id"];
+                                else if (canvas.images && canvas.images[0] && canvas.images[0].resource)
+                                    canvas.thumbnail = canvas.images[0].resource["@id"];
+                                canvases.push(canvas);
+                            });
+                        }
+                    });
+                    self.manifests.push(manifestData);
+                    return canvases;
+                };
+
+                var getManifestData = function(manifestURL) {
+                    window.fetch(manifestURL)
+                        .then(function(response) {
+                            return response.json();
+                        })
+                        .then(function(manifestData) {
+                            parseManifestJson(manifestData);
+                        })
+                };
+
+                const VisualWork_UsedImage_nodeid = '9743a1b2-8591-11ea-97eb-acde48001122';
+                const DigitalResource_Identifier_Content_nodeid = 'db05c421-ca7a-11e9-bd7a-a4d18cec433a';
+                const DigitalResource_Identifier_Type_nodeid = 'db05c05e-ca7a-11e9-8824-a4d18cec433a';
+                const URL_Concept_valueid = 'f32d0944-4229-4792-a33c-aadc2b181dc7';
+                this.visualWorkIds = resourceUtils.getNodeValues({
+                    widgetLabel: 'Shows Image.shows',
+                    returnTiles: false
+                }, this.report.get('tiles'), this.report.graph);
+                this.visualWorkIds.forEach(function(resourceid) {
+                    // look up related Visual Work
+                    resourceUtils.lookupResourceInstanceData(resourceid)
+                        .then(function(data) {
+                            var used_image_resourceids = resourceUtils.getNodeValues({
+                                nodeId: VisualWork_UsedImage_nodeid,
+                                returnTiles: false
+                            }, data._source.tiles);
+
+                            // look up related Digital Resource
+                            used_image_resourceids.forEach(function(resourceid) {
+                                resourceUtils.lookupResourceInstanceData(resourceid)
+                                    .then(function(data) {
+                                        // console.log(data)
+                                        var manifests = resourceUtils.getNodeValues({
+                                            nodeId: DigitalResource_Identifier_Content_nodeid,
+                                            where: {
+                                                nodeId: DigitalResource_Identifier_Type_nodeid,
+                                                contains: URL_Concept_valueid
+                                            },
+                                            returnTiles: false
+                                        }, data._source.tiles);
+                                        // console.log(manifests);
+                                        getManifestData(manifests);
+                                    });
+                            });
+                        });
+                });
             }
         },
         template: { require: 'text!templates/views/components/reports/physical-thing.htm' }
