4e89cf0f58fb970716741e11bfd9e8a1e4950fac
Cyrus Hiatt <chiatt@fargeo.com>
2020-06-03 16:16:08 -0700

    Removes cruft re #189

--- afs/media/js/views/components/workflows/iiif-step.js
+++ afs/media/js/views/components/workflows/iiif-step.js
@@ -8,16 +8,10 @@ define([
     'bindings/select2-query',
 ], function(_, $, arches, ko, koMapping, NewTileStep) {
     function viewModel(params) {
-        var self = this;
 
         params.resourceid(params.workflow.state.steps[params.physicalthingidstep()].physicalthingid);
         NewTileStep.apply(this, [params]);
         
-        this.digitalResourceId = ko.observable(null);
-        if (params.workflow.state.steps[params._index - 1]) {
-            this.digitalResourceId(params.workflow.state.steps[params._index - 1].physicalthingid);
-        }
-
         this.tile.subscribe(function(t){
             if (t) {
                 t.data["5d440fea-8651-11ea-97eb-acde48001122"](params.workflow.state.steps[params.visualworkidstep()].visualworkid); // set resourceid from related visual work
@@ -27,7 +21,7 @@ define([
 
         params.defineStateProperties = function(){
             return {
-                resourceid: self.digitalResourceId(),
+                resourceid: params.resourceid(),
                 tile: !!(ko.unwrap(params.tile)) ? koMapping.toJS(params.tile().data) : undefined,
                 tileid: !!(ko.unwrap(params.tile)) ? ko.unwrap(params.tile().tileid): undefined,
             };
