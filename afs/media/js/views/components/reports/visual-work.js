b4b3f8d5925551e18631d95644942d77675e0d15
njkim <nkim@fargeo.com>
2020-07-17 18:30:33 -0700

    Add edit button to visual work summary report, re #179

--- afs/media/js/views/components/reports/visual-work.js
+++ afs/media/js/views/components/reports/visual-work.js
@@ -6,7 +6,9 @@ define(['jquery', 'underscore', 'knockout', 'arches', 'viewmodels/tabbed-report'
             TabbedReportViewModel.apply(this, [params]);
 
             if (params.summary) {
-                // var resourceid = params.report.attributes.resourceid;
+
+                this.editorLink = arches.urls.resource_editor + this.report.attributes.resourceid;
+
                 var StatementTextId = 'e58ecc2e-c062-11e9-ba30-a4d18cec433a'; // ok
                 var TypeOfWorkId = '28a4ae07-c062-11e9-a11d-a4d18cec433a';
                 var DepictsPhysicalId = '5513933a-c062-11e9-9e4b-a4d18cec433a';
