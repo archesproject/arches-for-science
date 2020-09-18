06664c667af3586f96a21da8a48deccd35920aee
Galen Mancino <whatisgalen@gmail.com>
2020-09-17 18:21:30 -0700

    rm cardComponent as arg passed into button functions,  re #267

--- afs/media/js/views/components/workflows/alt-buttons.js
+++ afs/media/js/views/components/workflows/alt-buttons.js
@@ -13,26 +13,26 @@ define([
             {
                 "label": "Delete Alt",
                 "classes": "btn btn-shim btn-labeled btn-lg fa fa-trash",
-                "onClick": function(cardComp){if (ko.unwrap(cardComp.loading) !== true) cardComp.deleteTile();},
-                "visible": function(cardComp){return !!cardComp.tile ? !!cardComp.tile.tileid && !!cardComp.deleteTile : false;}
+                "onClick": function(){if (ko.unwrap(self.card.loading) !== true) self.card.deleteTile();},
+                "visible": function(){return !!self.card.tile ? !!self.card.tile.tileid && !!self.card.deleteTile : false;}
             },
             {
                 "label": "Cancel Alt",
                 "classes": "btn btn-shim btn-danger btn-labeled btn-lg fa fa-times",
-                "onClick": function(cardComp){if (cardComp.tile && ko.unwrap(cardComp.loading) !== true) {cardComp.tile.reset();}},
-                "visible": function(cardComp){return !!cardComp.provisionalTileViewModel && !cardComp.provisionalTileViewModel.tileIsFullyProvisional() && !!cardComp.card.isWritable && cardComp.tile.dirty();}
+                "onClick": function(){if (self.card.tile && ko.unwrap(self.card.loading) !== true) {self.card.tile.reset();}},
+                "visible": function(){return !!self.card.provisionalTileViewModel && !self.card.provisionalTileViewModel.tileIsFullyProvisional() && !!self.card.card.isWritable && self.card.tile.dirty();}
             },
             {
                 "label": "Save Alt",
                 "classes": "btn btn-shim btn-labeled btn-lg fa fa-plus",
-                "onClick": function(cardComp){if (ko.unwrap(cardComp.loading) !== true) cardComp.saveTile();},
-                "visible": function(cardComp){return !!cardComp.tile ? !!cardComp.tile.tileid : false;}
+                "onClick": function(){if (ko.unwrap(self.card.loading) !== true) self.card.saveTile();},
+                "visible": function(){return !!self.card.tile ? !!self.card.tile.tileid : false;}
             },
             {
                 "label": "Add Alt",
                 "classes": "btn btn-shim btn-labeled btn-lg btn-mint fa fa-plus",
-                "onClick": function(cardComp){if (ko.unwrap(cardComp.loading) !== true) cardComp.saveTile();},
-                "visible": function(cardComp){return !!cardComp.tile ? !cardComp.tile.tileid : true;}
+                "onClick": function(){if (ko.unwrap(self.card.loading) !== true) self.card.saveTile();},
+                "visible": function(){return !!self.card.tile ? !self.card.tile.tileid : true;}
             }
         ];
         self.getJSON();
