fd7a8a7533940add01be12b6ab98e2f487e82562
njkim <nkim@fargeo.com>
2020-11-11 14:07:56 -0800

    Support multiple canvases in a manifest, re #6838

--- afs/functions/file_to_iiif.py
+++ afs/functions/file_to_iiif.py
@@ -40,11 +40,11 @@ class FileToIIIF(BaseFunction):
         if not os.path.exists(CANTALOUPE_DIR):
             os.mkdir(CANTALOUPE_DIR)
 
+        canvases = []
         for f in files:
             if os.path.splitext(f.path.name)[1].lower() in acceptable_types:
                 dest = os.path.join(CANTALOUPE_DIR, os.path.basename(f.path.url))
                 file_name = os.path.basename(f.path.name)
-                file_name_less_ext = os.path.splitext(file_name)[0]
                 file_url = CANTALOUPE_HTTP_ENDPOINT + "iiif/2/" + file_name
                 file_json = file_url + "/info.json"
                 logger.info("copying file to local dir")
@@ -52,89 +52,90 @@ class FileToIIIF(BaseFunction):
                 image_json = self.fetch(file_json)
                 if image_json is None:
                     return
-                pres_dict = {
-                    "@context": "http://iiif.io/api/presentation/2/context.json",
-                    "@type": "sc:Manifest",
-                    "description": desc,
-                    "label": name,
-                    "logo": "",
-                    "metadata": [{"label": "TBD", "value": ["Unknown"]}],
-                    "thumbnail": {
-                        "@id": file_url + "/full/!300,300/0/default.jpg",
-                        "@type": "dctypes:Image",
-                        "format": "image/jpeg",
-                        "label": "Main VIew (.45v)",
-                    },
-                    "sequences": [
-                        {
-                            "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/sequence/TBD.json",
-                            "@type": "sc:Sequence",
-                            "canvases": [
-                                {
-                                    "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/canvas/TBD.json",
-                                    "@type": "sc:Canvas",
+                canvases.append(
+                    {
+                        "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/canvas/TBD.json",
+                        "@type": "sc:Canvas",
+                        "height": image_json["height"],
+                        "width": image_json["width"],
+                        "images": [
+                            {
+                                "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/annotation/TBD.json",
+                                "@type": "oa.Annotation",
+                                "motivation": "unknown",
+                                "on": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/canvas/TBD.json",
+                                "resource": {
+                                    "@id": file_url + "/full/full/0/default.jpg",
+                                    "@type": "dctypes:Image",
+                                    "format": "image/jpeg",
                                     "height": image_json["height"],
                                     "width": image_json["width"],
-                                    "images": [
-                                        {
-                                            "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/annotation/TBD.json",
-                                            "@type": "oa.Annotation",
-                                            "motivation": "unknown",
-                                            "on": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/canvas/TBD.json",
-                                            "resource": {
-                                                "@id": file_url + "/full/full/0/default.jpg",
-                                                "@type": "dctypes:Image",
-                                                "format": "image/jpeg",
-                                                "height": image_json["height"],
-                                                "width": image_json["width"],
-                                                "service": {
-                                                    "@context": "http://iiif.io/api/image/2/context.json",
-                                                    "@id": file_url,
-                                                    "profile": "http://iiif.io/api/image/2/level2.json",
-                                                },
-                                            },
-                                        }
-                                    ],
-                                    "label": name,
-                                    "license": "TBD",
-                                    "thumbnail": {
-                                        "@id": file_url + "/full/!300,300/0/default.jpg",
-                                        "@type": "dctypes:Image",
-                                        "format": "image/jpeg",
-                                        "service": {
-                                            "@context": "http://iiif.io/api/image/2/context.json",
-                                            "@id": file_url,
-                                            "profile": "http://iiif.io/api/image/2/level2.json",
-                                        },
+                                    "service": {
+                                        "@context": "http://iiif.io/api/image/2/context.json",
+                                        "@id": file_url,
+                                        "profile": "http://iiif.io/api/image/2/level2.json",
                                     },
-                                }
-                            ],
-                            "label": "Object",
-                            "startCanvas": "",
-                        }
-                    ],
-                }
-
-                manifest = models.IIIFManifest.objects.create(label=name, description=desc, manifest=pres_dict)
-                manifest_id = manifest.id
-                json_url = f"/manifest/{manifest_id}"
-                manifest.url = json_url
-                manifest.save()
-
-                # save the url to digital resource identifier_content node
-                manifest_url_nodeid = "db05c421-ca7a-11e9-bd7a-a4d18cec433a"
-                manifest_url_nodegroupid = "db05b5ca-ca7a-11e9-82ca-a4d18cec433a"
-                # set the identifier type node to "url" concept
-                digital_resource_identifier_type_nodeid = "db05c05e-ca7a-11e9-8824-a4d18cec433a"
-                url_concept_valueid = "f32d0944-4229-4792-a33c-aadc2b181dc7"
-                if not Tile.objects.filter(resourceinstance=tile.resourceinstance, nodegroup_id=manifest_url_nodegroupid).exists():
-                    url_tile = Tile()
-                    url_tile.nodegroup = models.NodeGroup.objects.get(nodegroupid=manifest_url_nodegroupid)
-                    url_tile.resourceinstance = tile.resourceinstance
-                    url_tile.data = {manifest_url_nodeid: json_url, digital_resource_identifier_type_nodeid: [url_concept_valueid]}
-                    url_tile.save()
+                                },
+                            }
+                        ],
+                        "label": f"{name} p. {len(canvases) + 1}",
+                        "license": "TBD",
+                        "thumbnail": {
+                            "@id": file_url + "/full/!300,300/0/default.jpg",
+                            "@type": "dctypes:Image",
+                            "format": "image/jpeg",
+                            "service": {
+                                "@context": "http://iiif.io/api/image/2/context.json",
+                                "@id": file_url,
+                                "profile": "http://iiif.io/api/image/2/level2.json",
+                            },
+                        },
+                    }
+                )
             else:
                 logger.warn("filetype unacceptable: " + f.path.name)
+        pres_dict = {
+            "@context": "http://iiif.io/api/presentation/2/context.json",
+            "@type": "sc:Manifest",
+            "description": desc,
+            "label": name,
+            "logo": "",
+            "metadata": [{"label": "TBD", "value": ["Unknown"]}],
+            "thumbnail": {
+                "@id": file_url + "/full/!300,300/0/default.jpg",
+                "@type": "dctypes:Image",
+                "format": "image/jpeg",
+                "label": "Main VIew (.45v)",
+            },
+            "sequences": [
+                {
+                    "@id": CANTALOUPE_HTTP_ENDPOINT + "iiif/manifest/sequence/TBD.json",
+                    "@type": "sc:Sequence",
+                    "canvases": canvases,
+                    "label": "Object",
+                    "startCanvas": "",
+                }
+            ],
+        }
+
+        manifest = models.IIIFManifest.objects.create(label=name, description=desc, manifest=pres_dict)
+        manifest_id = manifest.id
+        json_url = f"/manifest/{manifest_id}"
+        manifest.url = json_url
+        manifest.save()
+
+        # save the url to digital resource identifier_content node
+        manifest_url_nodeid = "db05c421-ca7a-11e9-bd7a-a4d18cec433a"
+        manifest_url_nodegroupid = "db05b5ca-ca7a-11e9-82ca-a4d18cec433a"
+        # set the identifier type node to "url" concept
+        digital_resource_identifier_type_nodeid = "db05c05e-ca7a-11e9-8824-a4d18cec433a"
+        url_concept_valueid = "f32d0944-4229-4792-a33c-aadc2b181dc7"
+        if not Tile.objects.filter(resourceinstance=tile.resourceinstance, nodegroup_id=manifest_url_nodegroupid).exists():
+            url_tile = Tile()
+            url_tile.nodegroup = models.NodeGroup.objects.get(nodegroupid=manifest_url_nodegroupid)
+            url_tile.resourceinstance = tile.resourceinstance
+            url_tile.data = {manifest_url_nodeid: json_url, digital_resource_identifier_type_nodeid: [url_concept_valueid]}
+            url_tile.save()
 
     def fetch(self, url):
         try:
