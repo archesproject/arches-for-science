5039a0baa080eecfe5cea885cd31db1e35b42a1c
Christopher Byrd <cbyrd@fargeo.com>
2020-12-09 15:56:13 -0800

    Update workflow params #284

--- afs/media/js/views/components/plugins/sample-workflow.js
+++ afs/media/js/views/components/plugins/sample-workflow.js
@@ -10,6 +10,38 @@ define([
             var self = this;
             params.steps = [
                 {
+                    title: 'Project Info',
+                    name: 'projectInfo',
+                    description: 'Information about the Project',
+                    component: 'views/components/workflows/new-step',
+                    componentname: 'new-step',
+                    graphid: '615b11ee-c457-11e9-910c-a4d18cec433a',  /* Observation */
+                    layoutSections: [
+                        {
+                            'sectionTitle': 'Project',
+                            'widgetConfigs': [
+                                { 
+                                    'widgetInstanceName': 'project-resource-select', /* unique to step */
+                                    'widgetid': '31f3728c-7613-11e7-a139-784f435179ea', /* resource-instance-select-widget */
+                                    'required': true,
+                                },
+                            ], 
+                        },
+                        {
+                            'sectionTitle': 'Sample Object',
+                            'widgetConfigs': [
+                                { 
+                                    'widgetInstanceName': 'sample-object-resource-select', /* unique to step */
+                                    'widgetid': '31f3728c-7613-11e7-a139-784f435179ea', /* resource-instance-select-widget */
+                                    'required': true,
+                                },
+                            ], 
+                        },
+                    ],
+                    required: true,
+                    icon: 'fa-search',
+                    class: 'fa-search',
+                    wastebin: {}
                 },
             ];
             Workflow.apply(this, [params]);
