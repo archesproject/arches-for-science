d99a7cfab2660bc0bd069a6bafbd4603dabbe061
Christopher Byrd <cbyrd@fargeo.com>
2021-03-23 14:09:13 -0700

    nit #329

--- afs/media/js/views/components/plugins/sample-workflow.js
+++ afs/media/js/views/components/plugins/sample-workflow.js
@@ -20,6 +20,7 @@ define([
                     },
                     component: 'views/components/workflows/component-based-step',
                     componentname: 'component-based-step',
+                    autoAdvance: false,
                     required: true,
                     layoutSections: [
                         {
@@ -34,6 +35,7 @@ define([
                                         ],
                                         renderContext: 'workflow',
                                     },
+                                    required: true,
                                 },
                             ], 
                         },
@@ -49,6 +51,7 @@ define([
                                         ],
                                         renderContext: 'workflow',
                                     },
+                                    required: true,
                                 },
                             ], 
                         },
