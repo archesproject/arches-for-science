2c4005e7b5d09336c747ab8ef1b4db81b110d90b
njkim <nkim@fargeo.com>
2021-05-04 14:38:27 -0700

    Add skeleton for the new sample-taking wrkflw #359

--- afs/media/js/views/components/plugins/sample-taking-workflow.js
+++ afs/media/js/views/components/plugins/sample-taking-workflow.js
@@ -13,67 +13,52 @@ define([
 
             params.steps = [
                 {
-                    title: 'Sample Name',
-                    name: 'object-sample-name',  /* unique to workflow */
-                    description: 'Provide a name for the sample',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
-                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
-                    nodegroupid: '0b926359-ca85-11e9-ac9c-a4d18cec433a',
+                    title: 'Project Info',
+                    name: 'select-project',  /* unique to workflow */
+                    description: 'Select the project and object',
+                    informationboxdata: {
+                        heading: 'Projects and related object',
+                        text: 'Select the project and object that your are sampling',
+                    },
+                    component: 'views/components/workflows/component-based-step',
+                    componentname: 'component-based-step',
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
-                    required: true,
-                    shouldtrackresource: true,
+                    required: false,
                     wastebin: {resourceid: null, description: 'a sampling activity instance'}
                 },
                 {
-                    title: 'Sample Date',
-                    name: 'object-sample-date',  /* unique to workflow */
+                    title: 'Sample Info',
+                    name: 'object-sample-info',  /* unique to workflow */
                     description: 'The date that the sample was taken',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
+                    informationboxdata: {
+                        heading: 'Sampling Information',
+                        text: 'Enter the people, date, method, and notes describing the sampling activities on the object',
+                    },
+                    component: 'views/components/workflows/component-based-step',
+                    componentname: 'component-based-step',
                     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
                     nodegroupid: '0b925e3a-ca85-11e9-a308-a4d18cec433a',
                     hiddenNodes: ['0b92f57d-ca85-11e9-a353-a4d18cec433a', '0b931623-ca85-11e9-b235-a4d18cec433a', '0b930905-ca85-11e9-8aca-a4d18cec433a'],
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
-                    required: true,
+                    required: false,
                 },
                 {
-                    title: 'Sample Taker',
-                    name: 'object-sample-taker',  /* unique to workflow */
-                    description: 'Who took the sample?',
-                    component: 'views/components/workflows/new-tile-step',
-                    componentname: 'new-tile-step',
-                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
-                    nodegroupid: 'dbaa2022-9ae7-11ea-ab62-dca90488358a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: true,
-                },
-                {
-                    title: 'Set or Collection',
-                    customCardLabel: 'Set or Collection Used in Sampling Activity',
-                    name: 'activity-collection',  /* unique to workflow */
-                    description: 'What set or collection is related?',
-                    component: 'views/components/workflows/related-set-step',
-                    componentname: 'related-set-step',
-                    graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
-                    nodegroupid: 'cc5d6df3-d477-11e9-9f59-a4d18cec433a',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: true,
-                },
-                {
-                    title: 'Select Physical Thing',
+                    title: 'Image',
                     name: 'select-physical-thing',  /* unique to workflow */
                     description: 'Select a physical thing from the set to annotate',
-                    component: 'views/components/workflows/physical-thing-list',
-                    componentname: 'physical-thing-list',
+                    informationboxdata: {
+                        heading: 'Image Services',
+                        text: 'Image services provide you with picture(s) of an object, often from multiple vantage points, \
+                        that can be annotated to indicate the location or region of an observation. \
+                        If you wish, you can upload photographs and automatically create a new image service \
+                        to document the location of your observations of an object',
+                    },
+                    component: 'views/components/workflows/component-based-step',
+                    componentname: 'component-based-step',
                     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
                     nodegroupid: '466f81d4-c451-11e9-b7c9-a4d18cec433a',
                     nodeid: '466fa421-c451-11e9-9a6d-a4d18cec433a',
@@ -83,14 +68,19 @@ define([
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
-                    required: true,
+                    required: false,
                 },
                 {
-                    title: 'IIIF',
+                    title: 'Sample Location',
                     name: 'iiif-step',  /* unique to workflow */
                     description: 'annotate image',
-                    component: 'views/components/workflows/iiif-step',
-                    componentname: 'iiif-step',
+                    informationboxdata: {
+                        heading: 'Draw Sample Locations',
+                        text: 'Draw the location of each sample that you took from the object. \
+                        Use the drawing tools to indicate a point, line, or polygon for each sample location',
+                    },
+                    component: 'views/components/workflows/component-based-step',
+                    componentname: 'component-based-step',
                     graphid: '9519cb4f-b25b-11e9-8c7b-a4d18cec433a',
                     nodegroupid: 'fec59582-8593-11ea-97eb-acde48001122',
                     externalstepdata: { 
@@ -100,15 +90,15 @@ define([
                     resourceid: null,
                     tileid: null,
                     parenttileid: null,
-                    required: true,
+                    required: false,
                     wastebin: {tile: null, description: 'an annotation'}
                 },
                 {
-                    title: 'Add Project Complete',
+                    title: 'Summary',
                     name: 'add-project-complete',  /* unique to workflow */
-                    description: 'Choose an option below',
-                    component: 'views/components/workflows/afs-final-step',
-                    componentname: 'afs-final-step',
+                    description: 'Summary',
+                    component: 'views/components/workflows/component-based-step',
+                    componentname: 'component-based-step',
                     graphid: '0b9235d9-ca85-11e9-9fa2-a4d18cec433a',
                     nodegroupid: '',
                     resourceid: null,
