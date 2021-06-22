7290fcde9dd485c651cfec983725de7e4de2255f
Christopher Byrd <cbyrd@fargeo.com>
2021-06-22 14:15:08 -0700

    update manifest manager #354

--- afs/media/js/views/components/workflows/workflow-manifest-manager.js
+++ afs/media/js/views/components/workflows/workflow-manifest-manager.js
@@ -4,8 +4,11 @@ define([
     'arches',
     'knockout',
     'knockout-mapping',
+    'models/graph',
+    'viewmodels/card',
+    'viewmodels/provisional-tile',
     'views/components/plugins/manifest-manager',
-], function(_, $, arches, ko, koMapping) {
+], function(_, $, arches, ko, koMapping, GraphModel, CardViewModel, ProvisionalTileViewModel) {
     function viewModel(params) {
         var self = this;
 
@@ -41,7 +44,6 @@ define([
         console.log("AAA", self)
 
         params.saveFunction(function() {
-            /* saving a tile without a resourceinstance_id creates a new resources */ 
             self.digitalResourceNameTile.save().then(function(data) {
 
 
@@ -51,6 +53,9 @@ define([
 
                 self.digitalResourceStatementTile.save().then(function(data) {
                     console.log("digitalResourceStatementData", data, self.digitalResourceStatementTile, params)
+
+                    console.log("b", self, this)
+
 
                     self.updatePhysicalThingWithDigitalResource(data).then(function(data) {
                         console.log("updatePhysicalThingData", data)
@@ -67,16 +72,115 @@ define([
         };
 
         this.updatePhysicalThingWithDigitalResource = function(digitalResourceData) {
-            return $.ajax({
-                url: arches.urls.api_node_value,
-                type: 'POST',
-                data: {
-                    'resourceinstanceid': self.physicalThingData.resourceId,
-                    'nodeid': 'a298ee52-8d59-11eb-a9c4-faffc265b501', // Digital Source (E73) (physical thing)
-                    'data': digitalResourceData,
-                    'tileid': null,
+            console.log("in updatePhysicalTHingWIthDR", digitalResourceData, self, this)
+
+
+
+            $.getJSON( arches.urls.api_card + self.physicalThingData.resourceId ).then(function(data) {
+                var digitalReferenceCardData = data.cards.find(function(card) {
+                    return card.nodegroup_id === '8a4ad932-8d59-11eb-a9c4-faffc265b501';
+                });
+
+                var handlers = {
+                    'after-update': [],
+                    'tile-reset': []
+                };
+
+                var graphModel = new GraphModel({
+                    data: {
+                        nodes: data.nodes,
+                        nodegroups: data.nodegroups,
+                        edges: []
+                    },
+                    datatypes: data.datatypes
+                });
+
+                // var provisionalTileViewModel = new ProvisionalTileViewModel({
+                //     tile: null,
+                //     reviewer: data.userisreviewer
+                // });
+
+                var digitalReferenceCard = new CardViewModel({
+                    card: digitalReferenceCardData,
+                    graphModel: graphModel,
+                    tile: null,
+                    resourceId: ko.observable(digitalResourceData.resourceinstance_id),
+                    displayname: data.displayname,
+                    handlers: handlers,
+                    cards: data.cards,
+                    tiles: data.tiles,
+                    // provisionalTileViewModel: provisionalTileViewModel,
+                    cardwidgets: data.cardwidgets,
+                    userisreviewer: data.userisreviewer,
+                });
+
+                var digitalReferenceTile = digitalReferenceCard.getNewTile();
+
+                var digitalSourceNodeId = 'a298ee52-8d59-11eb-a9c4-faffc265b501'; // Digital Source (E73) (physical thing)
+                digitalReferenceTile.data[digitalSourceNodeId] = {
+                    "resourceId": self.physicalThingData.resourceId,
+                    "ontologyProperty": "http://www.cidoc-crm.org/cidoc-crm/P67i_is_referred_to_by",
+                    "inverseOntologyProperty": "http://www.cidoc-crm.org/cidoc-crm/P67_refers_to"
                 }
-            })
+                
+                var digitalReferenceTypeNodeId = 'f11e4d60-8d59-11eb-a9c4-faffc265b501'; // Digital Reference Type (E55) (physical thing)
+                digitalReferenceTile.data[digitalReferenceTypeNodeId] = '1497d15a-1c3b-4ee9-a259-846bbab012ed' // Preferred Manifest concept
+
+
+                console.log("digitalReferenceTile", digitalReferenceTile)
+
+                digitalReferenceTile.save().then(function(tileData) {
+                    console.log("dsds tiledata", tileData)
+
+                //     var resourceInstanceIdFrom = self.physicalThingData.resourceId;
+                //     var resourceInstanceIdTo = digitalResourceData.resourceinstance_id;
+        
+                //     var relationshipType = 'http://www.cidoc-crm.org/cidoc-crm/P67i_is_referred_to_by';
+                //     var inverseRelationshipType = 'http://www.cidoc-crm.org/cidoc-crm/P67_refers_to';
+        
+                //     var nodeId = 'a298ee52-8d59-11eb-a9c4-faffc265b501'; // Digital Source (E73) (physical thing)
+        
+                //     $.ajax({
+                //         url: arches.urls.api_related_resources,
+                //         type: 'POST',
+                //         data: {
+                //             'resourceinstanceid_from': resourceInstanceIdFrom,
+                //             'resourceinstanceid_to': resourceInstanceIdTo,
+                //             'relationship_type': relationshipType,
+                //             'inverse_relationship_type': inverseRelationshipType,
+                //             'node_id': nodeId,
+                //             'tile_id': tileData.tileid,
+                //         }
+                //     }).then(function(data) {
+                //         console.log('in importn resp', data)
+                //     })
+                })
+
+                
+
+
+
+
+                // tile.save().then(function(tileData) {
+
+                // });
+
+
+
+                
+                console.log('dsoi', data, foo, tile)
+            });
+
+            // return $.ajax({
+            //     url: arches.urls.api_node_value,
+            //     type: 'POST',
+            //     data: {
+            //         'resourceinstanceid': self.physicalThingData.resourceId,
+            //         'nodeid': 'a298ee52-8d59-11eb-a9c4-faffc265b501', // Digital Source (E73) (physical thing)
+            //         'data': digitalResourceData,
+            //         'tileid': null,
+            //     }
+            // })
         };
 
         this.initialize();
