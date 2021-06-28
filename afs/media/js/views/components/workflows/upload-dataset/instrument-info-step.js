14daa616fb1c6ad21b60291d2ba6da15e5bae130
Cyrus Hiatt <chiatt@fargeo.com>
2021-06-28 13:54:35 -0700

    Update variable declaration, re #289

--- afs/media/js/views/components/workflows/upload-dataset/instrument-info-step.js
+++ afs/media/js/views/components/workflows/upload-dataset/instrument-info-step.js
@@ -9,20 +9,20 @@ define([
     function viewModel(params) {
 
         var self = this;
-        var instrumentNodeId = '1acc9d59-c458-11e9-99e4-a4d18cec433a';
-        var procedureNodeId =  '51416e9c-c458-11e9-b70e-a4d18cec433a';
-        var parameterNodeId = '8ec331a1-c457-11e9-8d7a-a4d18cec433a';
-        var parameterNodeGroupId = '8ec30d3a-c457-11e9-81dc-a4d18cec433a'; // parameter are 'Statement' cards
-        var nameNodeGroupId = '87e3d6a1-c457-11e9-9ec9-a4d18cec433a';
-        var nameNodeId = '87e40cc5-c457-11e9-8933-a4d18cec433a';
-        var projectInfo = params.form.externalStepData.projectinfo.data['select-phys-thing-step'][0][1];
-        var physThingName = projectInfo.physThingName;
-        var observedThingNodeId = 'cd412ac5-c457-11e9-9644-a4d18cec433a';
-        var observedThingInstanceId = projectInfo.physicalThing;
-        var projectInstanceId = projectInfo.project;
-        var projectNodeId = '736f06a4-c54d-11ea-9f58-024e0d439fdb';
+        const instrumentNodeId = '1acc9d59-c458-11e9-99e4-a4d18cec433a';
+        const procedureNodeId =  '51416e9c-c458-11e9-b70e-a4d18cec433a';
+        const parameterNodeId = '8ec331a1-c457-11e9-8d7a-a4d18cec433a';
+        const parameterNodeGroupId = '8ec30d3a-c457-11e9-81dc-a4d18cec433a'; // parameter are 'Statement' cards
+        const nameNodeGroupId = '87e3d6a1-c457-11e9-9ec9-a4d18cec433a';
+        const nameNodeId = '87e40cc5-c457-11e9-8933-a4d18cec433a';
+        const projectInfo = params.form.externalStepData.projectinfo.data['select-phys-thing-step'][0][1];
+        const physThingName = projectInfo.physThingName;
+        const observedThingNodeId = 'cd412ac5-c457-11e9-9644-a4d18cec433a';
+        const observedThingInstanceId = projectInfo.physicalThing;
+        const projectInstanceId = projectInfo.project;
+        const projectNodeId = '736f06a4-c54d-11ea-9f58-024e0d439fdb';
 
-        var getProp = function(key, prop) {
+        const getProp = function(key, prop) {
             if (ko.unwrap(params.value) && params.value()[key])
                 return params.value()[key][prop] || params.value()[key];
             else {
@@ -30,12 +30,12 @@ define([
             } 
         };
 
-        var parameterTileId = getProp('parameter', 'tileid');
-        var instrumentTileId = getProp('instrument', 'tileid');
-        var procedureTileId = getProp('procedure', 'tileid');
-        var projectTileId = getProp('project', 'tileid');
-        var observedThingTileid = getProp('observedThing', 'tileid');
-        var nameTileId = getProp('name', 'tileid');
+        let parameterTileId = getProp('parameter', 'tileid');
+        let instrumentTileId = getProp('instrument', 'tileid');
+        let procedureTileId = getProp('procedure', 'tileid');
+        let projectTileId = getProp('project', 'tileid');
+        let observedThingTileid = getProp('observedThing', 'tileid');
+        let nameTileId = getProp('name', 'tileid');
 
         this.instrumentValue = ko.observable(getProp('instrument', 'value'));
         this.procedureValue = ko.observable(getProp('procedure', 'value'));
@@ -57,7 +57,7 @@ define([
 
         this.instrumentValue.subscribe(function(val){
             if (val) {
-                var instrumentData = resourceUtils.lookupResourceInstanceData(val);
+                let instrumentData = resourceUtils.lookupResourceInstanceData(val);
                 self.instrumentInstance(self.createRelatedInstance(val));
                 instrumentData.then(function(data){
                     self.nameValue("Observation of " + physThingName + " with " + data._source.displayname);
@@ -86,7 +86,7 @@ define([
         });
 
         this.buildTile = function(data, nodeGroupId, resourceid, tileid) {
-            var res = {
+            let res = {
                 "tileid": tileid || "",
                 "nodegroup_id": nodeGroupId,
                 "parenttile_id": null,
@@ -100,7 +100,7 @@ define([
         };
 
         this.saveTile = function(data, nodeGroupId, resourceid, tileid) {
-            var tile = self.buildTile(data, nodeGroupId, resourceid, tileid);
+            let tile = self.buildTile(data, nodeGroupId, resourceid, tileid);
             return window.fetch(arches.urls.api_tiles(tileid || uuid.generate()), {
                 method: 'POST',
                 credentials: 'include',
@@ -116,35 +116,35 @@ define([
         };
 
         params.form.save = function() {
-            var observedThingData = {};
+            let observedThingData = {};
             observedThingData[observedThingNodeId] = self.createRelatedInstance(observedThingInstanceId);
             return self.saveTile(observedThingData, observedThingNodeId, self.observationInstanceId(), observedThingTileid)
                 .then(function(data) {
-                    var partOfProjectData = {};
+                    let partOfProjectData = {};
                     observedThingTileid = data.tileid;
                     partOfProjectData[projectNodeId] = self.createRelatedInstance(projectInstanceId);
                     return self.saveTile(partOfProjectData, projectNodeId, data.resourceinstance_id, projectTileId);
                 })
                 .then(function(data) {
-                    var nameData = {};
+                    let nameData = {};
                     nameData[nameNodeId] = self.nameValue();
                     projectTileId = data.tileid;
                     return self.saveTile(nameData, nameNodeGroupId, data.resourceinstance_id, nameTileId);
                 })
                 .then(function(data) {
-                    var instrumentData = {};
+                    let instrumentData = {};
                     instrumentData[instrumentNodeId] = self.instrumentInstance();
                     nameTileId = data.tileid;
                     return self.saveTile(instrumentData, instrumentNodeId, data.resourceinstance_id, instrumentTileId);
                 })
                 .then(function(data) {
-                    var procedureData = {};
+                    let procedureData = {};
                     procedureData[procedureNodeId] = self.procedureInstance();
                     instrumentTileId = data.tileid;
                     return self.saveTile(procedureData, procedureNodeId, data.resourceinstance_id, procedureTileId);
                 })
                 .then(function(data) {
-                    var parameterData = {};
+                    let parameterData = {};
                     parameterData[parameterNodeId] = self.parameterValue();
                     procedureTileId = data.tileid;
                     return self.saveTile(parameterData, parameterNodeGroupId, data.resourceinstance_id, parameterTileId);
