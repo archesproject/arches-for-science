5d46c12c8bf14ba845141244a2d86ce179b30003
Christopher Byrd <cbyrd@fargeo.com>
2021-07-09 14:18:37 -0700

    nit #406

--- afs/media/js/views/components/plugins/sample-taking-workflow.js
+++ afs/media/js/views/components/plugins/sample-taking-workflow.js
@@ -6,6 +6,7 @@ define([
     'viewmodels/workflow-step',
     'views/components/workflows/select-phys-thing-step',
     'views/components/workflows/sample-taking-workflow/sampling-info-step',
+    'views/components/workflows/sample-taking-workflow/sample-taking-sample-location',
     'views/components/workflows/sample-taking-workflow/sample-taking-final-step'
 ], function(ko, $, arches, Workflow) {
     return ko.components.register('sample-taking-workflow', {
@@ -109,22 +110,27 @@ define([
                 },
                 {
                     title: 'Sample Location',
-                    name: 'iiif-step',  /* unique to workflow */
-                    description: 'annotate image',
-                    informationboxdata: {
-                        heading: 'Draw Sample Locations',
-                        text: 'Draw the location of each sample that you took from the object. \
-                        Use the drawing tools to indicate a point, line, or polygon for each sample location',
-                    },
+                    name: 'sample-location-step', /* unique to workflow */
                     component: 'views/components/workflows/component-based-step',
                     componentname: 'component-based-step',
-                    graphid: '9519cb4f-b25b-11e9-8c7b-a4d18cec433a',
-                    nodegroupid: 'fec59582-8593-11ea-97eb-acde48001122',
-                    resourceid: null,
-                    tileid: null,
-                    parenttileid: null,
-                    required: false,
-                    wastebin: {tile: null, description: 'an annotation'}
+                    required: true,
+                    externalstepdata: {
+                    },
+                    workflowstepclass: 'analysis-areas-workflow-regions-step',
+                    layoutSections: [
+                        {
+                            componentConfigs: [
+                                { 
+                                    componentName: 'sample-taking-sample-location',
+                                    uniqueInstanceName: 'sample-location-instance', /* unique to step */
+                                    tilesManaged: 'one',
+                                    parameters: {
+                                        graphid: '9519cb4f-b25b-11e9-8c7b-a4d18cec433a',  /* physical thing */
+                                    },
+                                },
+                            ], 
+                        },
+                    ],
                 },
                 {
                     title: 'Summary',
