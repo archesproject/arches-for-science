4554ad420fa9d72e1cc47f03071bd2143bd796d3
njkim <nkim@fargeo.com>
2021-08-05 11:53:40 +0900

    nit, #355

--- afs/media/js/views/components/workflows/review-dataset/select-dataset.js
+++ afs/media/js/views/components/workflows/review-dataset/select-dataset.js
@@ -14,43 +14,43 @@ define([
             url: arches.urls.related_resources + this.physicalThingResourceId(),
             context: this,
             dataType: 'json',
-            }).done(function(data) {
-                self.relatedDigitalResources(data.related_resources.related_resources
-                    .filter(function(related_resource) {
-                        return related_resource.graph_id == '707cbd78-ca7a-11e9-990b-a4d18cec433a'})
-                )
+        }).done(function(data) {
+            self.relatedDigitalResources(data.related_resources.related_resources
+                .filter(function(related_resource) {
+                    return related_resource.graph_id == '707cbd78-ca7a-11e9-990b-a4d18cec433a'})
+            );
 
-                self.relatedDigitalResources().forEach(function(resource) {
-                    resource.selected = ko.observable().extend({ deferred: true });
-                    if(params.value()) {
-                        params.value().find(function(val) {
-                            if (val.resourceid == resource.resourceinstanceid) {
-                                resource.selected(true);
-                            }
-                            else {
-                                resource.selected(false);
-                            }
-                        });
-                    }
-                    self.dataLoaded(true);
-                })
+            self.relatedDigitalResources().forEach(function(resource) {
+                resource.selected = ko.observable().extend({ deferred: true });
+                if(params.value()) {
+                    params.value().find(function(val) {
+                        if (val.resourceid == resource.resourceinstanceid) {
+                            resource.selected(true);
+                        }
+                        else {
+                            resource.selected(false);
+                        }
+                    });
+                }
+                self.dataLoaded(true);
+            });
 
-                self.selectedDigtalResources = ko.pureComputed(function() {
-                        return self.relatedDigitalResources().map(function(resource){
-                            return {
-                                resourceid: resource.resourceinstanceid,
-                                selected: resource.selected()
-                            }
-                        })
-                    })
+            self.selectedDigtalResources = ko.pureComputed(function() {
+                    return self.relatedDigitalResources().map(function(resource){
+                        return {
+                            resourceid: resource.resourceinstanceid,
+                            selected: resource.selected()
+                        };
+                    });
+                });
 
-                self.selectedDigtalResources.subscribe(function(val) {
-                    params.value(val);
-                })
+            self.selectedDigtalResources.subscribe(function(val) {
+                params.value(val);
+            });
 
 
 
-        })
+        });
 
     }
 
