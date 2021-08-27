c0a6606b51e2ba3e34be53b0b766dcd38cfcc697
Christopher Byrd <cbyrd@fargeo.com>
2021-08-27 11:21:01 -0700

    updates analysis-areas workflow #569

--- afs/media/js/views/components/plugins/analysis-areas-workflow.js
+++ afs/media/js/views/components/plugins/analysis-areas-workflow.js
@@ -17,6 +17,7 @@ define([
                 {
                     title: 'Object',
                     name: 'object-step', /* unique to workflow */
+                    required: true,
                     informationboxdata: {
                         heading: 'Workflow Goal: Record Locations and Regions of Interest',
                         text: `
@@ -25,7 +26,6 @@ define([
                             This workflow will guide you through the steps to document the location of your regions of interest.
                         `,
                     },
-                    required: true,
                     layoutSections: [
                         {
                             sectionTitle: 'Object or Sample',
@@ -38,7 +38,6 @@ define([
                                         graphids: [
                                             '9519cb4f-b25b-11e9-8c7b-a4d18cec433a',  /* physical thing */
                                         ],
-                                        renderContext: 'workflow'
                                     },
                                 },
                             ], 
@@ -48,6 +47,7 @@ define([
                 {
                     title: 'Image',
                     name: 'image-step', /* unique to workflow */
+                    required: true,
                     informationboxdata: {
                         heading: 'Image Services',
                         text: `
@@ -55,11 +55,7 @@ define([
                             If you wish, you can upload photographs and automatically create a new image service to document the location of your observations of an object.
                         `,
                     },
-                    required: true,
                     lockableExternalSteps: ['object-step'],
-                    externalstepdata: {
-                        objectstep: 'object-step'
-                    },
                     layoutSections: [
                         {
                             sectionTitle: 'Image Service',
@@ -69,7 +65,8 @@ define([
                                     uniqueInstanceName: 'image-service-instance', /* unique to step */
                                     tilesManaged: 'one',
                                     parameters: {
-                                        graphid: '707cbd78-ca7a-11e9-990b-a4d18cec433a'  /* Digital Resources */
+                                        graphid: '707cbd78-ca7a-11e9-990b-a4d18cec433a',  /* Digital Resources */
+                                        physicalThingResourceId: "['object-step']['sample-object-resource-instance'][0][1]"
                                     },
                                 },
                             ], 
@@ -80,12 +77,8 @@ define([
                     title: 'Regions',
                     name: 'regions-step', /* unique to workflow */
                     required: true,
-                    lockableExternalSteps: ['image-step'],
-                    externalstepdata: {
-                        objectstep: 'object-step',
-                        imagestep: 'image-step',
-                    },
                     workflowstepclass: 'analysis-areas-workflow-regions-step',
+                    lockableExternalSteps: ['image-step'],
                     layoutSections: [
                         {
                             componentConfigs: [
@@ -95,6 +88,8 @@ define([
                                     tilesManaged: 'one',
                                     parameters: {
                                         graphid: '9519cb4f-b25b-11e9-8c7b-a4d18cec433a',  /* physical thing */
+                                        physicalThingResourceId: "['object-step']['sample-object-resource-instance'][0][1]",
+                                        imageStepData: "['image-step']['image-service-instance'][0]['data']"
                                     },
                                 },
                             ], 
@@ -105,10 +100,6 @@ define([
                     title: 'Summary',
                     name: 'analysis-areas-complete',  /* unique to workflow */
                     description: 'Summary',
-                    externalstepdata: {
-                        objectstep: 'object-step',
-                        regionsstep: 'regions-step',
-                    },
                     layoutSections: [
                         {
                             componentConfigs: [
@@ -117,6 +108,8 @@ define([
                                     uniqueInstanceName: 'analysis-areas-final',
                                     tilesManaged: 'none',
                                     parameters: {
+                                        sampleObjectResourceId: "['object-step']['sample-object-resource-instance'][0][1]",
+                                        regionsStepData: "['regions-step']['annotation-instance']"
                                     },
                                 },
                             ], 
