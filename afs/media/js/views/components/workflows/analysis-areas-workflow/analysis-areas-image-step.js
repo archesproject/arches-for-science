accc47b9213e669d5aa39525acebd86df0291b1c
njkim <nkim@fargeo.com>
2021-09-07 13:35:55 -0700

    Add loading while getting preferred manifest, #585

--- afs/media/js/views/components/workflows/analysis-areas-workflow/analysis-areas-image-step.js
+++ afs/media/js/views/components/workflows/analysis-areas-workflow/analysis-areas-image-step.js
@@ -10,7 +10,6 @@ define([
 ], function(_, $, arches, ko, koMapping, GraphModel, CardViewModel) {
     function viewModel(params) {
         var self = this;
-        params.pageVm.loading(true);
         this.isManifestManagerHidden = ko.observable(true);
         this.shouldShowEditService = ko.observable(false);
 
@@ -270,8 +269,9 @@ define([
 
                 if (digitalReferenceTypeValue === ( preferredManifestConceptValueId || alternateManifestConceptValueId ))  {
                     var physicalThingManifestResourceId = tile.data[digitalSourceNodeId]()[0].resourceId();
-
-                    $.getJSON( arches.urls.api_card + physicalThingManifestResourceId ).then(function(data) {
+                    params.pageVm.loading(true);
+                    $.getJSON( arches.urls.api_card + physicalThingManifestResourceId )
+                    .then(function(data) {
                         if (digitalReferenceTypeValue === preferredManifestConceptValueId) {
                             self.physicalThingDigitalReferencePreferredManifestResourceData.push(data);
                         }
@@ -286,6 +286,8 @@ define([
                         else if (!self.selectedPhysicalThingImageServiceName()) {
                             self.selectedPhysicalThingImageServiceName(self.physicalThingDigitalReferencePreferredManifestResourceData()[0].displayname);
                         }
+                    })
+                    .always(function() {
                         params.pageVm.loading(false);
                     });
                 }
