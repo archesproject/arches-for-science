ea35c159dd85b5d826bc9ce80b4f214fb591d2e0
Aaron Gundel <aaron.gundel@gmail.com>
2021-09-16 10:33:44 -0600

    lowercase the dataconfig name

--- afs/media/js/views/components/reports/scenes/name.js
+++ afs/media/js/views/components/reports/scenes/name.js
@@ -49,11 +49,11 @@ define(['underscore', 'knockout', 'arches', 'utils/report','bindings/datatable']
                 } 
 
                 self.names(nameData.map(x => {
-                    const type = self.getNodeValue(x, `${self.dataConfig.name}_type`);
-                    const content = self.getNodeValue(x, `${self.dataConfig.name}_content`);
-                    const language = self.getNodeValue(x, `${self.dataConfig.name}_language`);
-                    const label = self.getNodeValue(x, `${self.dataConfig.name}_label`);
-                    const source = self.getNodeValue(x, `${self.dataConfig.name}_source`);
+                    const type = self.getNodeValue(x, `${self.dataConfig.name.toLowerCase()}_type`);
+                    const content = self.getNodeValue(x, `${self.dataConfig.name.toLowerCase()}_content`);
+                    const language = self.getNodeValue(x, `${self.dataConfig.name.toLowerCase()}_language`);
+                    const label = self.getNodeValue(x, `${self.dataConfig.name.toLowerCase()}_label`);
+                    const source = self.getNodeValue(x, `${self.dataConfig.name.toLowerCase()}_source`);
                     const tileid = x?.['@tile_id'];
                     return { type, content, language, label, source, tileid }
                 }));
