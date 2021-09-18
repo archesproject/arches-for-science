4148995274f222f610fef256ede511b04639a338
Cyrus Hiatt <chiatt@fargeo.com>
2021-09-18 12:25:15 -0700

    Divides sample locations and analysis regions into separate layers and shows popup for analysis areas. re #520

--- afs/media/js/views/components/workflows/sample-taking-workflow/sample-taking-sample-location-step.js
+++ afs/media/js/views/components/workflows/sample-taking-workflow/sample-taking-sample-location-step.js
@@ -8,7 +8,8 @@ define([
     'models/graph',
     'viewmodels/card',
     'views/components/iiif-annotation',
-], function(_, $, arches, ko, koMapping, ResourceUtils, GraphModel, CardViewModel, IIIFAnnotationViewmodel) {
+    'text!templates/views/components/iiif-popup.htm',
+], function(_, $, arches, ko, koMapping, ResourceUtils, GraphModel, CardViewModel, IIIFAnnotationViewmodel, iiifPopup) {
     function viewModel(params) {
         var self = this;
         _.extend(this, params);
@@ -89,7 +90,11 @@ define([
 
         this.sampleLocationFilterTerm = ko.observable();
         this.filteredSampleLocationInstances = ko.computed(function() {
-            const samplesOnly = self.sampleLocationInstances().filter(function(a){return !self.analysisAreaResourceIds.includes(a.data[partIdentifierAssignmentPhysicalPartOfObjectNodeId]()[0].resourceId())})
+            const samplesOnly = self.sampleLocationInstances().filter(function(a){
+                const analysisAreaResourceIds = self.analysisAreaResourceIds.map(item => item.resourceid);
+                const partId = ko.unwrap(a.data[partIdentifierAssignmentPhysicalPartOfObjectNodeId]()[0].resourceId)
+                return !analysisAreaResourceIds.includes(partId);
+            });
             if (self.sampleLocationFilterTerm()) {
                 return samplesOnly.filter(function(sampleLocationInstance) {
                     var partIdentifierAssignmentLabelNodeId = '3e541cc6-859b-11ea-97eb-acde48001122';
@@ -193,7 +198,7 @@ define([
         this.removeFeatureFromCanvas = function(feature) {
             var annotationNodes = self.annotationNodes();
             
-            var physicalThingAnnotationNodeName = "Physical Thing - Part Identifier Assignment_Polygon Identifier";
+            var physicalThingAnnotationNodeName = "Sample Locations";
             var physicalThingAnnotationNode = annotationNodes.find(function(annotationNode) {
                 return annotationNode.name === physicalThingAnnotationNodeName;
             });
@@ -219,7 +224,7 @@ define([
             var annotationNodes = self.annotationNodes();
             
             if (self.selectedSampleLocationInstanceFeatures()) {
-                var physicalThingAnnotationNodeName = "Physical Thing - Part Identifier Assignment_Polygon Identifier";
+                var physicalThingAnnotationNodeName = "Sample Locations";
                 var physicalThingAnnotationNode = annotationNodes.find(function(annotationNode) {
                     return annotationNode.name === physicalThingAnnotationNodeName;
                 });
@@ -250,7 +255,7 @@ define([
         };
 
         this.updateSampleLocationInstances = function() {
-            var physicalThingAnnotationNodeName = "Physical Thing - Part Identifier Assignment_Polygon Identifier";
+            var physicalThingAnnotationNodeName = "Sample Locations";
             var physicalThingAnnotationNode = self.annotationNodes().find(function(annotationNode) {
                 return annotationNode.name === physicalThingAnnotationNodeName;
             });
@@ -362,7 +367,7 @@ define([
         };
 
         this.setPhysicalThingGeometriesToVisible = function(annotationNodes) {
-            var physicalThingAnnotationNodeName = "Physical Thing - Part Identifier Assignment_Polygon Identifier";
+            var physicalThingAnnotationNodeName = "Sample Locations";
             var physicalThingAnnotationNode = annotationNodes.find(function(annotationNode) {
                 return annotationNode.name === physicalThingAnnotationNodeName;
             });
@@ -446,7 +451,7 @@ define([
             var updateAnnotations = function() {
                 return new Promise(function(resolve, _reject) {
                     /* updates selected annotations */ 
-                    var physicalThingAnnotationNodeName = "Physical Thing - Part Identifier Assignment_Polygon Identifier";
+                    var physicalThingAnnotationNodeName = "Sample Locations";
                     var physicalThingAnnotationNode = self.annotationNodes().find(function(annotationNode) {
                         return annotationNode.name === physicalThingAnnotationNodeName;
                     });
@@ -816,8 +821,14 @@ define([
 
             const classificationNodeId = '8ddfe3ab-b31d-11e9-aff0-a4d18cec433a';
             const analysisAreaTypeConceptId = '19ef147a-3703-41b4-bfa3-d4c66e333b0b'; //'this is actually the ceramic value as a placeholder'
-            const related = card.tiles().map(tile => ko.unwrap(tile.data[partIdentifierAssignmentPhysicalPartOfObjectNodeId])[0].resourceId())
-            Promise.all(related.map(resourceid => ResourceUtils.lookupResourceInstanceData(resourceid))).then((values) => {
+            const related = card.tiles().map((tile) => {
+                return {
+                    'resourceid': ko.unwrap(tile.data[partIdentifierAssignmentPhysicalPartOfObjectNodeId])[0].resourceId(),
+                    'tileid': tile.tileid
+                }
+            });
+
+            Promise.all(related.map(resource => ResourceUtils.lookupResourceInstanceData(resource.resourceid))).then((values) => {
                 values.forEach((value) => {
                     const nodevals = ResourceUtils.getNodeValues({
                         nodeId: classificationNodeId,
@@ -827,10 +838,11 @@ define([
                         }
                     }, value._source.tiles);
                     if (nodevals.includes(analysisAreaTypeConceptId)) {
-                        self.analysisAreaResourceIds.push(value._id);
+                        self.analysisAreaResourceIds.push(related.find(tile => value._id === tile.resourceid));
                     }
                 });
                 self.sampleLocationInstances(card.tiles());
+                self.analysisAreaTileIds = self.analysisAreaResourceIds.map(item => item.tileid);
             })
             /* 
                 subscription to features lives here because we _only_ want it to run once, on blank starting tile, when a user places a feature on the map
@@ -868,6 +880,42 @@ define([
                         self.featureLayers.push(layer)
                     }
 
+                    if (self.analysisAreaTileIds.includes(feature.properties.tileId)){
+                        const analysisArea = self.analysisAreaResourceIds.find(analysisArea => analysisArea.tileid === feature.properties.tileId);
+                        var popup = L.popup({
+                            closeButton: false,
+                            maxWidth: 349
+                        })
+                            .setContent(iiifPopup)
+                            .on('add', function() {
+                                var popupData = {
+                                    'closePopup': function() {
+                                        popup.remove();
+                                    },
+                                    'name': ko.observable(''),
+                                    'description': ko.observable(''),
+                                    'graphName': feature.properties.graphName,
+                                    'resourceinstanceid': analysisArea.resourceid,
+                                    'reportURL': arches.urls.resource_report
+                                };
+                                window.fetch(arches.urls.resource_descriptors + popupData.resourceinstanceid)
+                                    .then(function(response) {
+                                        return response.json();
+                                    })
+                                    .then(function(descriptors) {
+                                        popupData.name(descriptors.displayname);
+                                        const description = `<strong>Analysis Area</strong>
+                                            <br>Analysis Areas may not be modified in the sample taking workflow
+                                            <br>${descriptors['map_popup'] !== "Undefined" ? descriptors['map_popup'] : ''}`
+                                        popupData.description(description);
+                                    });
+                                var popupElement = popup.getElement()
+                                    .querySelector('.mapboxgl-popup-content');
+                                ko.applyBindingsToDescendants(popupData, popupElement);
+                            });
+                        layer.bindPopup(popup);
+                    }
+
                     layer.on({
                         click: function() {
 
@@ -894,8 +942,68 @@ define([
                             } 
                         },
                     })
+                },
+                buildAnnotationNodes: function(json) {
+                    let sampleAnnotations = ko.observableArray();
+                    let analysisAreaAnnotations = ko.observableArray();
+                    var updateAnnotations = function() {
+                        var canvas = self.canvas();
+                        if (canvas) {
+                            window.fetch(arches.urls.iiifannotations + '?canvas=' + canvas + '&nodeid=' + partIdentifierAssignmentPolygonIdentifierNodeId)
+                                .then(function(response) {
+                                    return response.json();
+                                })
+                                .then(function(json) {
+                                    json.features.forEach(function(feature) {
+                                        feature.properties.graphName = "Physical Thing";
+                                        if (self.analysisAreaTileIds.includes(feature.properties.tileId)) {
+                                            feature.properties.type = 'analysis_area';
+                                            feature.properties.color = '#AC53F5';
+                                            feature.properties.fillColor = '#AC53F5';
+                                            analysisAreaAnnotations.push(feature);
+                                        } else {
+                                            feature.properties.type = 'sample_location';
+                                            sampleAnnotations.push(feature);
+                                        }
+                                    });
+                                    const editNodeActiveState = ko.observable(false);
+                                    editNodeActiveState.subscribe(function(active){
+                                        if (!active) {
+                                            self.resetSampleLocationTile();
+                                        }
+                                    });
+                                    self.annotationNodes([
+                                        {
+                                            name: "Sample Locations",
+                                            icon: "fa fa-eyedropper",
+                                            active: editNodeActiveState,
+                                            opacity: ko.observable(100),
+                                            annotations: sampleAnnotations
+                                        },
+                                        {
+                                            name: "Analysis Areas",
+                                            icon: "fa fa-eye",
+                                            active: ko.observable(false),
+                                            opacity: ko.observable(100),
+                                            annotations: analysisAreaAnnotations
+                                        },
+                                    ])
+                                });
+                        }
+                    };
+                    self.canvas.subscribe(updateAnnotations);
+                    updateAnnotations();
                 }
             }]);
+
+            this.map.subscribe(function(map){
+                map.on('click', function(e){
+                    const clickedFeatureLayer = $(e.originalEvent.path[0]).hasClass('leaflet-interactive')
+                    if (!clickedFeatureLayer) {
+                        self.resetSampleLocationTile()
+                    }
+                })
+            });
 
             /* overwrites iiif-annotation method */ 
             self.updateTiles = function() {
