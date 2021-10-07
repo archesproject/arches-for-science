af3cec4760b16fee2fcdeea61927430c10873738
Cyrus Hiatt <chiatt@fargeo.com>
2021-10-07 16:44:06 -0700

    If thing already has collection tile, add current collection relationship to existing relationships. re #671

--- afs/media/js/views/components/workflows/stepUtils.js
+++ afs/media/js/views/components/workflows/stepUtils.js
@@ -1,17 +1,44 @@
 define(['arches', 'uuid', 'utils/resource'], function(arches, uuid, ResourceUtils) {
     return {
         saveThingToProject: async function(physicalThingInstanceId, projectSetInstanceId, workflowId, resourceLookup){
+            let thing;
+            let tileId = uuid.generate();
+            let alreadySaved = false;
             const memberOfSetNodeid = '63e49254-c444-11e9-afbe-a4d18cec433a';
-            const tileId = uuid.generate();
             const data = {};
+
             data[memberOfSetNodeid] = [{
                 "resourceId": projectSetInstanceId,
                 "ontologyProperty": "",
                 "resourceXresourceId": "",
                 "inverseOntologyProperty": ""
             }];
+
+            if (resourceLookup[physicalThingInstanceId]) {
+                thing = resourceLookup[physicalThingInstanceId];
+            } else {
+                thing = await ResourceUtils.lookupResourceInstanceData(physicalThingInstanceId);
+            }
+
+            const tile = thing._source.tiles.find((tile) => {
+                return tile.nodegroup_id === memberOfSetNodeid;
+            });
+
+            if (tile) {
+                // check if the tile already has a relationship to the current project's collection
+                if (tile.data[memberOfSetNodeid].find(val => val.resourceId === projectSetInstanceId)) {
+                    alreadySaved = true;
+                } else {
+                    // if not, add the new collection relationship to existing relationships
+                    tileId = tile.tileid;
+                    if (Array.isArray(tile.data[memberOfSetNodeid])) {
+                        data[memberOfSetNodeid] = data[memberOfSetNodeid].concat(tile.data[memberOfSetNodeid]);
+                    }
+                }
+            }
+
             const tileObj = {
-                "tileid": "",
+                "tileid": tileId,
                 "data": data,
                 "nodegroup_id": memberOfSetNodeid,
                 "parenttile_id": null,
@@ -21,19 +48,7 @@ define(['arches', 'uuid', 'utils/resource'], function(arches, uuid, ResourceUtil
                 "transaction_id": workflowId
             };
 
-            let thing;
-            if (resourceLookup[physicalThingInstanceId]) {
-                thing = resourceLookup[physicalThingInstanceId];
-            } else {
-                thing = await ResourceUtils.lookupResourceInstanceData(physicalThingInstanceId);
-            }
-
-            const res = thing._source.tiles.find((tile) => {
-                return tile.nodegroup_id === memberOfSetNodeid &&
-                    tile.data[memberOfSetNodeid].find(val => val.resourceId === projectSetInstanceId);
-            });
-
-            if (!res) {
+            if (!alreadySaved) {
                 return window.fetch(arches.urls.api_tiles(tileId), {
                     method: 'POST',
                     credentials: 'include',
