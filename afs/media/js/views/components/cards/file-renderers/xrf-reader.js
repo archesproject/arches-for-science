af54bc08562ee970657c25e0333a353026e565c7
njkim <nkim@fargeo.com>
2021-10-14 14:08:50 -0700

    Add axis labels in the xrf reader, #675

--- afs/media/js/views/components/cards/file-renderers/xrf-reader.js
+++ afs/media/js/views/components/cards/file-renderers/xrf-reader.js
@@ -17,6 +17,9 @@ define(['jquery',
                     }
                 });
             };
+            this.chartTitle("XRF Spectrum");
+            this.xAxisLabel("Energy");
+            this.yAxisLabel("Counts");
         },
         template: { require: 'text!templates/views/components/cards/file-renderers/afs-reader.htm' }
     });
