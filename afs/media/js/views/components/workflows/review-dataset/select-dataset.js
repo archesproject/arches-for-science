a4ce020c915ef5d14c761b983a137cdde808d965
Cyrus Hiatt <chiatt@fargeo.com>
2021-11-10 17:41:28 -0800

    Add new view to get digital resources, re #781

--- afs/media/js/views/components/workflows/review-dataset/select-dataset.js
+++ afs/media/js/views/components/workflows/review-dataset/select-dataset.js
@@ -7,46 +7,40 @@ define([
         var self = this;
         this.digitalResourceGraphId = '707cbd78-ca7a-11e9-990b-a4d18cec433a';
         this.physicalThingResourceId = ko.observable(params.physicalThingResourceId);
-        this.relatedDigitalResources = ko.observableArray([]);
+        this.relatedDigitalResources = ko.observableArray();
         this.dataLoaded = ko.observable(false);
 
-        $.ajax({
-            url: arches.urls.related_resources + this.physicalThingResourceId(),
-            context: this,
-            dataType: 'json',
-        }).done(function(data) {
-            self.relatedDigitalResources(data.related_resources.related_resources
-                .filter(function(relatedResource) {
-                    return relatedResource.graph_id === self.digitalResourceGraphId;
-                }));
-
-            self.relatedDigitalResources().forEach(function(resource) {
-                resource.selected = ko.observable();
-                if(params.value()) {
-                    params.value().forEach(function(val) {
-                        if (val.resourceid === resource.resourceinstanceid) {
-                            resource.selected(val.selected);
-                        }
-                    });
-                }
+        const getDigitalResources = async function(resourceid) {
+            const url = `${arches.urls.root}digital-resources-by-object-parts/${resourceid}`;
+            const result = await fetch(url, {
+                method: 'GET',
+                credentials: 'include'
             });
+            if (result.ok) {
+                const results = await result.json();
+                const resources = results.resources;
+                resources.forEach(resource => resource.selected = ko.observable(false));
+                self.relatedDigitalResources(resources);
+                self.dataLoaded(true);
+            }
+        };
 
-            self.selectedDigtalResources = ko.pureComputed(function() {
-                return self.relatedDigitalResources().map(function(resource){
-                    return {
-                        resourceid: resource.resourceinstanceid,
-                        selected: resource.selected()
-                    };
-                });
+        this.selectedDigtalResources = ko.pureComputed(function() {
+            return self.relatedDigitalResources().map(function(resource){
+                return {
+                    resourceid: resource.resourceid,
+                    selected: resource.selected()
+                };
             });
-
-            self.selectedDigtalResources.subscribe(function(val) {
-                params.value(val);
-            });
-
-            self.dataLoaded(true);
         });
 
+        this.selectedDigtalResources.subscribe(function(val) {
+            const data = {digitalResources: val, resourceid: self.physicalThingResourceId()};
+            params.value(data);
+        });
+
+        getDigitalResources(params.physicalThingResourceId);
+        this.physicalThingResourceId.subscribe(getDigitalResources);
     }
 
     ko.components.register('select-dataset', {
