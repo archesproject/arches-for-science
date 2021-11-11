d17586483ffc2295c3d97abaf71d6663690aadb7
Cyrus Hiatt <chiatt@fargeo.com>
2021-11-11 09:53:51 -0800

    Assign resourceid of the part to the steps saved data. re #781

--- afs/views/digital_resources_by_object_parts.py
+++ afs/views/digital_resources_by_object_parts.py
@@ -15,10 +15,13 @@ class DigitalResourcesByObjectParts(View):
         physical_things = [tile.data[physical_thing_node_id][0]["resourceId"] for tile in part_tiles]
         physical_things.append(resourceid)
         digital_resource_tiles = TileModel.objects.filter(resourceinstance_id__in=physical_things).filter(nodegroup_id=digital_resource_nodegroup_id)
-
         related_datasets = {tile.data[digital_resource_node_id][0]["resourceId"] for tile in digital_resource_tiles}
+        part_lookup = {tile.data[digital_resource_node_id][0]["resourceId"]: str(tile.resourceinstance_id) for tile in digital_resource_tiles}
         digital_resource_type_tiles = TileModel.objects.filter(resourceinstance_id__in=related_datasets).filter(nodegroup_id=digital_resource_type_nodegroup_id)
         manifest_datasets = {str(tile.resourceinstance_id) for tile in digital_resource_type_tiles if manifest_concept_value_id in tile.data[digital_resource_type_nodegroup_id]}
         digital_resources = Resource.objects.filter(pk__in=related_datasets - manifest_datasets)
         results = {"resources":[{"resourceid":str(resource.pk), "displayname":resource.displayname} for resource in digital_resources], "selected_resource": resourceid}
+        for resource in results["resources"]:
+            resource["partresourceid"] = part_lookup[resource["resourceid"]]
+            resource["isparent"] = part_lookup[resource["resourceid"]] == resourceid
         return JSONResponse(results)
