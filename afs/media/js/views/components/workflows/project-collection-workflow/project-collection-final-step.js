327bb45fd3dacedf0209b8c4d4bf1731039b4dc3
njkim <nkim@fargeo.com>
2022-01-24 16:28:33 -0800

    Update final step to separate child things, #834

--- afs/media/js/views/components/workflows/project-collection-workflow/project-collection-final-step.js
+++ afs/media/js/views/components/workflows/project-collection-workflow/project-collection-final-step.js
@@ -9,22 +9,42 @@ define([
         SummaryStep.apply(this, [params]);
 
         this.projectResourceId = params.projectResourceId;
-        this.collectionOfPhysThings = ko.observableArray();
+        this.collectionOfParentPhysThings = ko.observableArray();
+        this.collectionOfChildPhysThings = ko.observableArray();
 
         this.relatedResources.subscribe(function(val){
             const physicalThingGraphId = '9519cb4f-b25b-11e9-8c7b-a4d18cec433a';
-            self.collectionName = val["resource_instance"]["displayname"];
-            val["related_resources"].forEach(function(rr){
-                if (rr.graph_id === physicalThingGraphId) {
-                    self.collectionOfPhysThings.push({
-                        resourceid: rr.resourceinstanceid,
-                        name: rr.displayname,
-                    });
-                }
-                if (rr.resourceinstanceid === params.projectResourceId){
-                    self.projectName = rr.displayname;
-                }
-            });
+            const childPhysicalThingsValueIds = [
+                '77d8cf19-ce9c-4e0a-bde1-9148d870e11c', //sample
+                '7375a6fb-0bfb-4bcf-81a3-6180cdd26123', //sample location
+                '31d97bdd-f10f-4a26-958c-69cb5ab69af1', //anlysis area
+            ];
+
+            self.collectionName = self.getResourceValue(val, ["resource_instance","displayname"]);
+            self.projectName = val["related_resources"].find(rr =>
+                rr.resourceinstanceid === self.projectResourceId
+            ).displayname;
+
+            const collectionOfPhysThings = val['related_resources'].filter(rr =>
+                rr.graph_id === "9519cb4f-b25b-11e9-8c7b-a4d18cec433a"
+            );
+
+            self.collectionOfParentPhysThings(collectionOfPhysThings.filter(rr =>
+                !rr.tiles.find(tile =>
+                    tile.nodegroup_id === "8ddfe3ab-b31d-11e9-aff0-a4d18cec433a" &&
+                    tile.data["8ddfe3ab-b31d-11e9-aff0-a4d18cec433a"].find(value =>
+                        childPhysicalThingsValueIds.includes(value))
+                )
+            ).map(rr => {return {'name': rr.displayname, resourceid: rr.resourceinstanceid}}));
+
+            self.collectionOfChildPhysThings(collectionOfPhysThings.filter(rr =>
+                rr.tiles.find(tile =>
+                    tile.nodegroup_id === "8ddfe3ab-b31d-11e9-aff0-a4d18cec433a" &&
+                    tile.data["8ddfe3ab-b31d-11e9-aff0-a4d18cec433a"].find(value =>
+                        childPhysicalThingsValueIds.includes(value))
+                )
+            ).map(rr => {return {'name': rr.displayname, resourceid: rr.resourceinstanceid}}));
+
             this.loading(false);
         }, this);
     }
