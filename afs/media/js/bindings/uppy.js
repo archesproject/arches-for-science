0ccbf91446be3b49254f4655c0f686a2360f6c81
Aaron Gundel <aaron.gundel@gmail.com>
2022-07-08 15:54:47 -0600

    first attempt at client side/chunked uploads to s3

---
+++ afs/media/js/bindings/uppy.js
@@ -0,0 +1,39 @@
+define([
+    'jquery',
+    'underscore',
+    'knockout',
+    'js-cookie'
+], function($, _, ko, Cookies) {
+    /**
+     * @constructor
+     * @name dropzone
+     */
+    ko.bindingHandlers.uppy = {
+        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            var innerBindingContext = bindingContext.extend(valueAccessor);
+            ko.applyBindingsToDescendants(innerBindingContext, element);
+            var options = valueAccessor() || {};
+            const UppyContainer = window.Uppy;
+            const uppy = new UppyContainer.Core({debug: true, autoProceed: true}).use(UppyContainer.DragDrop, {
+                inline: options.inline,
+                target: element,
+                autoProceed: true,
+                logger: UppyContainer.Core.debugLogger,
+            }).use(UppyContainer.AwsS3Multipart, {
+                companionUrl: "/",
+                companionHeaders: {
+                    'X-CSRFToken': Cookies.get('csrftoken')
+                }
+            }).use(UppyContainer.ProgressBar, {
+                target: ".uppy-progress"
+            });
+            
+            if(options.filesAdded) {
+                uppy.on('complete', options.filesAdded);
+            }
+
+            return { controlsDescendantBindings: true };
+        }
+    };
+    return ko.bindingHandlers.dropzone;
+});
--- afs/urls.py
+++ afs/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from arches.app.views.plugin import PluginView
 from afs.views.physical_thing_search import PhysicalThingSearchView
 from afs.views.physical_things_in_set import PhysicalThingSetView
+from afs.views.s3 import S3MultipartUploadManagerView, S3MultipartUploaderView, batch_sign, complete_upload, upload_part
 from afs.views.update_resource_list import UpdateResourceListView
 from afs.views.digital_resources_by_object_parts import DigitalResourcesByObjectParts
 
@@ -19,4 +20,9 @@ urlpatterns = [
         name="digital-resources-by-object-parts",
     ),
     url(r"^updateresourcelist", UpdateResourceListView.as_view(), name="updateresourcelist"),
+    url(r"^s3/multipart$", S3MultipartUploaderView.as_view(), name="s3_multipart_upload"),
+    url(r"^s3/multipart/(?P<uploadid>[^\/]+)/complete$", complete_upload, name="s3_multipart_upload_complete"),
+    url(r"^s3/multipart/(?P<uploadid>[^\/]+)/batch", batch_sign, name="s3_multipart_batch_sign"),
+    url(r"^s3/multipart/(?P<uploadid>[^\/]+)/(?P<partnumber>\d+)$", upload_part, name="s3_multipart_upload_part"),
+    url(r"^s3/multipart/(?P<uploadid>[^\/]+)", S3MultipartUploadManagerView.as_view(), name="s3_multipart_upload"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
