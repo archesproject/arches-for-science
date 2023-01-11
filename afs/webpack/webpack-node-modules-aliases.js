b176f4ac014f82b5184263cdba747f94f335546a
Cyrus Hiatt <chiatt@fargeo.com>
2023-01-11 10:50:33 -0800

    Move module urls to webpack-node-modules-aliases, re #1057

---
+++ afs/webpack/webpack-node-modules-aliases.js
@@ -0,0 +1,13 @@
+module.exports = {
+    PROJECT_NODE_MODULES_ALIASES: JSON.stringify({
+        "plotly.js-dist": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/plotly.js-dist`)",
+        "Plotly": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/plotly.js-dist`)",
+        "three": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three`)",
+        "CSS2DRenderer": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/renderers/CSS2DRenderer`)",
+        "TrackballControls": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/controls/TrackballControls`)",
+        "PDBLoader": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/loaders/PDBLoader`)",
+        "PCDLoader": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/loaders/PCDLoader`)",
+        "ColladaLoader": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/loaders/ColladaLoader`)",
+        "TWEEN": "Path.resolve(__dirname, `${APP_ROOT}/media/node_modules/three/examples/js/loaders/TWEEN`)",
+    }),
+};
