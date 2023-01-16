6ad6e54acb718a88872ad7b783f12549afdeaaf3
njkim <nkim@fargeo.com>
2023-01-16 13:51:12 -0800

    nit, #1079

--- afs/media/js/reports/default.js
+++ afs/media/js/reports/default.js
@@ -2,7 +2,7 @@ define([
     'knockout',
     'viewmodels/report',
     'templates/views/report-templates/default.htm'
-], function (ko, ReportViewModel, defatultReportTemplate) {
+], function(ko, ReportViewModel, defatultReportTemplate) {
     return ko.components.register('default-report', {
         viewModel: function(params) {
             params.configKeys = [];
