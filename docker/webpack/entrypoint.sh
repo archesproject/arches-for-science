e9d4d44a3d9d090928fb9f643577f83e347447d6
Cyrus Hiatt <chiatt@fargeo.com>
2023-01-20 17:15:37 -0800

    Update afs to 7.4.x

--- docker/webpack/entrypoint.sh
+++ docker/webpack/entrypoint.sh
@@ -7,7 +7,7 @@ run_webpack() {
 	echo ""
 	cd ${APP_FOLDER}
     echo "Running Webpack"
-	exec sh -c "cd /web_root/afs/afs && yarn install && wait-for-it afs7-2:8000 -t 1200 && yarn start"
+	exec sh -c "cd /web_root/afs/afs && yarn install && wait-for-it afs7-4:8000 -t 1200 && yarn start"
 }
 
 run_webpack
