0cec6dd747ac8963a879b0c8226ce63e08559d4d
Cyrus Hiatt <chiatt@fargeo.com>
2023-01-25 13:46:44 -0800

    Add test to open graph designer

--- afs/cypress/e2e/spec.cy.js
+++ afs/cypress/e2e/spec.cy.js
@@ -2,9 +2,6 @@ describe('template spec', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:8009')
-  })
-  
-  it('authenticates dev user', () => {
     const user = 'dev';
     const pw = 'dev';
     cy.viewport(1050, 700);
@@ -13,6 +10,18 @@ describe('template spec', () => {
     cy.contains('Sign in').click();
     cy.get('[name="username"]').type(`${user}{enter}`)
     cy.get('[name="password"]').type(`${pw}{enter}`)
-    cy.contains('Log off')
   })
+  
+  it('creates a graph', () => {
+    cy.contains('Manage').click();
+    // cy.url().should('include', 'resource')
+    cy.get('.graph-designer-nav').click();
+    cy.get('.btn.btn-primary.dropdown-toggle').click();
+    cy.get('.new-resource-btn').click();
+  })
+  
+  // it('log off dev user', () => {
+  //   cy.contains('Log off').click();
+  //   cy.contains('Sign in').click();
+  // })
 })
