09099ef7fc600d9db7ea7b657ced17d59e05d988
Cyrus Hiatt <chiatt@fargeo.com>
2023-02-04 17:11:12 -0800

    Creates and publishes a graph

--- afs/cypress/e2e/spec.cy.js
+++ afs/cypress/e2e/spec.cy.js
@@ -14,10 +14,31 @@ describe('template spec', () => {
   
   it('creates a graph', () => {
     cy.contains('Manage').click();
-    // cy.url().should('include', 'resource')
-    cy.get('.graph-designer-nav').click();
+    cy.contains('Arches Designer').click();
     cy.get('.btn.btn-primary.dropdown-toggle').click();
-    cy.get('.new-resource-btn').click();
+    cy.get('.clickme').click();
+    cy.get('[data-original-title="Add Child Node"]').click();
+    cy.contains('New Node').click();
+    
+    //open chosen datatype select and select 'string'
+    cy.get(':nth-child(4) > .widgets > .row > .form-group > .crm-selector > .chosen-container > .chosen-single > span').click();
+    cy.get('.active-result').contains('string').click();
+
+    //save the newly added node
+    cy.contains('Save Edits').click();
+    
+    //click the add child node button of the newly added node
+    cy.get('[data-original-title="Add Child Node"]').eq(1).click();
+    cy.contains('New Node_1').click();
+    
+    //open chosen datatype select and select 'number'
+    cy.get(':nth-child(4) > .widgets > .row > .form-group > .crm-selector > .chosen-container > .chosen-single > span').click();
+    cy.get('.active-result').contains('number').click();
+
+    //save the newly added node and publish the graph
+    cy.contains('Save Edits').click();
+    cy.contains('Publish Graph').click();
+    cy.contains('Publish').click()
   })
   
   // it('log off dev user', () => {
