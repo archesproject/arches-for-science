599c94ce377c07da1cf23d098f10bd719bdcfda7
njkim <nkim@fargeo.com>
2023-05-10 10:59:38 -0700

    nit the error response, #1136

--- afs/views/download_project_files.py
+++ afs/views/download_project_files.py
@@ -34,8 +34,10 @@ class FileDownloader(View):
             ).format(len(files))
             return JSONResponse({"success": True, "message": message})
         else:
-            message = _("The Celery is required to download files. Or you can download file one at a time")
-            response = {"title": _("Error"), "message": message}
+            response = {
+                "title": _("Error"),
+                "message": _("The Celery is required to download files.")
+            }
             return JSONErrorResponse(content=response)
 
     def create_download_zipfile(self, user, files, project_name):
