ec3dd56236800e375098d9648040fde2fd26bd59
Johnathan Clementi <johnathan.clementi1@gmail.com>
2023-05-15 18:17:15 -0400

    Handle different template formats #1163

--- afs/media/js/views/components/workflows/project-report-workflow/report-template-select.js
+++ afs/media/js/views/components/workflows/project-report-workflow/report-template-select.js
@@ -9,21 +9,45 @@ define([
         var self = this;
         this.templateValue = ko.observable();
 
-        this.templates = ko.observableArray();
+        let arches_templates = [];
+        this.allTemplateFormats = ko.observableArray();
+        this.pptTemplates = ko.observableArray();
+        this.docxTemplates = ko.observableArray(); 
+        // this.xlsxTemplates = ko.observableArray();
+
         this.getTemplates = async () => {
             try {
                 const response = await fetch(arches.urls.reports_list);
                 const data = await response.json();
-                this.templates(data.map(template => {
-                    return {
-                        id: template.templateid,
-                        name: template.name,
-                        description: template.description,
-                        template: template,
-                        preview: `/files/${template.preview}`,
-                        thumbnail: `/files/${template.thumbnail}`,
-                    };
-                }));
+                arches_templates = data.map(template => {
+                        return {
+                            id: template.templateid,
+                            name: template.name,
+                            format: template.template.split('.').pop(),
+                            description: template.description,
+                            template: template,
+                            preview: `/files/${template.preview}`,
+                            thumbnail: `/files/${template.thumbnail}`,
+                        };
+                });
+                this.pptTemplates(arches_templates.filter(template => template.format === 'pptx'));
+                this.docxTemplates(arches_templates.filter(template => template.format === 'docx'));
+                // this.xlsxTemplates(arches_templates.filter(template => template.format === 'xlsx'));
+
+                this.allTemplateFormats([
+                    {
+                        heading: 'Word Templates',
+                        templates: this.pptTemplates()
+                    },
+                    {
+                        heading: 'Powerpoint Templates',
+                        templates: this.docxTemplates()
+                    },
+                    // {
+                    //     heading: 'Excel Templates',
+                    //     templates: this.xlsxTemplates()
+                    // },
+                ])
             } catch (error) {
                 console.error('Error: ', error);
             }
