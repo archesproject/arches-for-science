92202bc9f5646532a36cc166d3fc962a5917fa61
Jacob Walls <jwalls@fargeo.com>
2023-09-01 16:42:46 -0400

    Python packaging and Django changes

--- arches_for_science/celery.py
+++ arches_for_science/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'afs.settings')
-app = Celery('afs')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arches_for_science.settings')
+app = Celery('arches_for_science')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
