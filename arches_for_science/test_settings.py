a47659ffb9dc139aa08666b15c59e7bfef4e50fb
Jacob Walls <jwalls@fargeo.com>
2023-09-12 16:12:42 -0400

    Simplify settings import

--- arches_for_science/test_settings.py
+++ arches_for_science/test_settings.py
@@ -3,9 +3,7 @@ import os
 # Depends on /arches being on the python path, as arches.tests is not importable
 from tests.test_settings import *
 
-INSTALLED_APPS += ("arches_for_science", "arches_templating")
-ROOT_URLCONF = "arches_for_science.urls"
-APP_NAME = "arches_for_science"
+from arches_for_science.settings import INSTALLED_APPS, ROOT_URLCONF, APP_NAME
 APP_ROOT = os.path.dirname(__file__)
 
 # Further settings may need to be added from project, just don't
