988d6406d3da108e92d33cef8b9dba217febc656
Christopher Byrd <cbyrd@fargeo.com>
2023-10-18 13:25:06 -0700

    adds incomplete workflows modal to init-workflow plugin #1364

--- arches_for_science/media/js/views/components/plugins/init-workflow.js
+++ arches_for_science/media/js/views/components/plugins/init-workflow.js
@@ -2,14 +2,35 @@ define([
     'knockout',
     'arches',
     'afs-settings',
+    'viewmodels/alert-json',
     'templates/views/components/plugins/init-workflow.htm'
-], function(ko, arches, afsSettings, initWorkflowTemplate) {
+], function(ko, arches, afsSettings, JsonErrorAlertViewModel, initWorkflowTemplate) {
 
     var InitWorkflow = function(params) {
         this.workflows = params.workflows.map(function(wf){
             wf.url = arches.urls.plugin(wf.slug);
             return wf;
         }, this);
+
+        this.shouldShowIncompleteWorkflowsModal = ko.observable(false);
+
+        this.incompleteWorkflows = ko.observableArray([]);
+        this.incompleteWorkflows.subscribe(incompleteWorkflows => {
+            if (incompleteWorkflows.length) {
+                this.shouldShowIncompleteWorkflowsModal(true);
+            }
+        });
+
+        fetch(arches.urls.api_user_incomplete_workflows).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            else {
+                params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON))
+            }
+        }).then(respJSON => {
+            this.incompleteWorkflows(respJSON['incomplete_workflows']);
+        });
 
         // filters out the chemical image workflow, if cloud storage is not enabled.
         if(!afsSettings.cloudStorage.enabled){
