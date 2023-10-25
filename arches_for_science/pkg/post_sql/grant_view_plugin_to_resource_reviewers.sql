583c40f86b533088cd837f30e63084b58a1d3f8d
Jacob Walls <jwalls@fargeo.com>
2023-10-25 12:36:26 -0400

    Add migration granting Resource Reviewers view plugin access (#1366)

---
+++ arches_for_science/pkg/post_sql/grant_view_plugin_to_resource_reviewers.sql
@@ -0,0 +1,15 @@
+INSERT INTO guardian_groupobjectpermission
+    ("object_pk", "content_type_id", "group_id", "permission_id")
+VALUES
+    ('6f707d86-d49c-4ece-9883-8cbb2ecda1b5', 51, 9, 205),
+    ('c206cfc6-6b4a-481e-a018-8da72aeb7074', 51, 9, 205),
+    ('49507fb0-89c6-47b7-b506-9b2b29a3b8d8', 51, 9, 205),
+    ('a1667717-b7bd-4570-b27a-ec352c767e0e', 51, 9, 205),
+    ('1132c953-8af4-4acb-87c5-39c5b996799e', 51, 9, 205),
+    ('730be1cd-0575-44d7-be68-1316c345a8fb', 51, 9, 205),
+    ('afedd8cd-1ba7-43ec-b3c5-d0fab03525ca', 51, 9, 205),
+    ('847db730-4dad-4f61-b2bb-32abd62c0378', 51, 9, 205),
+    ('ef3a374c-f795-4317-aaee-83e1493eabb6', 51, 9, 205),
+    ('29512c4e-ed06-4a6b-ae2d-e423817b3f6e', 51, 9, 205),
+    ('7720e9fa-876c-4127-a77a-b099cd2a5d45', 51, 9, 205)
+ON CONFLICT DO NOTHING;
