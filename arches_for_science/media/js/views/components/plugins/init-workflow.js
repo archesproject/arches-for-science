b9bf904b5ada0f519986670d801f205ebbf47433
Christopher Byrd <cbyrd@fargeo.com>
2023-11-03 17:49:34 -0700

    adds help templates to workflows #1407

--- arches_for_science/media/js/views/components/plugins/init-workflow.js
+++ arches_for_science/media/js/views/components/plugins/init-workflow.js
@@ -7,16 +7,57 @@ define([
 ], function(ko, arches, afsSettings, JsonErrorAlertViewModel, initWorkflowTemplate) {
 
     var InitWorkflow = function(params) {
-        this.workflows = params.workflows.map(function(wf){
-            wf.url = arches.urls.plugin(wf.slug);
-            return wf;
-        }, this);
+        this.workflows = ko.observableArray([]);
+        this.helpTemplateData = ko.observableArray([]);
+        
+        fetch(arches.urls.api_plugins).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            }
+            else {
+                params.alert(new JsonErrorAlertViewModel('ep-alert-red', resp.responseJSON))
+            }
+        }).then(respJSON => {
+            let workflows = respJSON.reduce((acc, plugin) => {
+                if (plugin.config.is_workflow) {
+                    plugin.url = arches.urls.plugin(plugin.slug);
+                    acc.push(plugin);
+                }
+                return acc;
+            }, []);
 
-        this.shouldShowIncompleteWorkflowsModal = ko.observable(false);
+            // filters out the chemical analysis and chemical image workflows if cloud storage is not enabled.
+            if(!afsSettings.cloudStorage.enabled){
+                workflows = workflows.filter(
+                    workflow => {
+                        return !['c206cfc6-6b4a-481e-a018-8da72aeb7074', "af06e949-5e16-49f0-b23e-e8529e8ce321"].includes(workflow.pluginid);
+                    }
+                );
+            }
+
+            this.workflows(workflows);
+            this.helpTemplateData(workflows.reduce((acc, workflow) => {
+                if (workflow.helptemplate) {
+                    acc.push({'text': workflow.name, 'id': workflow.helptemplate})
+                }
+
+                return acc;
+            }, []))
+        });
+
         this.shouldShowWorkflowHelp = ko.observable(false);
-
+        this.helpTemplateUrl = ko.observable();
         this.selectedHelpTemplate = ko.observable();
+        this.selectedHelpTemplate.subscribe(helpTemplateName => {
+            if (helpTemplateName) {
+                this.helpTemplateUrl(arches.urls.help_template + `?template=${helpTemplateName}`)
+            }
+            else {
+                this.helpTemplateUrl(null);
+            }
+        })
 
+        this.shouldShowIncompleteWorkflowsModal = ko.observable(false);        
         this.incompleteWorkflows = ko.observableArray([]);
         this.incompleteWorkflows.subscribe(incompleteWorkflows => {
             if (incompleteWorkflows.length) {
@@ -39,11 +80,6 @@ define([
                 return workflowData;
             }));
         });
-
-        // filters out the chemical image workflow, if cloud storage is not enabled.
-        if(!afsSettings.cloudStorage.enabled){
-            this.workflows = this.workflows.filter(workflow => workflow.workflowid != 'c206cfc6-6b4a-481e-a018-8da72aeb7074');
-        }
     };
 
     return ko.components.register('init-workflow', {
