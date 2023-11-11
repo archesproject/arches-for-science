3b4acafcefff5b195bea25c79f82cad09cf38d7d
njkim <nkim@fargeo.com>
2023-11-11 03:27:11 -0800

    Add resource-manifest lookup, #1438

--- arches_for_science/models.py
+++ arches_for_science/models.py
@@ -1,6 +1,13 @@
 import uuid
 from django.db import models
 from django.db.models import JSONField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import get_language, get_language_bidi
+from arches.app.models.models import IIIFManifest
+from arches.app.models.resource import Resource
+from arches.app.models.tile import Tile
+
 
 class RendererConfig(models.Model):
     configid = models.UUIDField(primary_key=True, unique=True)
@@ -18,3 +25,13 @@ class RendererConfig(models.Model):
         super(RendererConfig, self).__init__(*args, **kwargs)
         if not self.configid:
             self.configid = uuid.uuid4()
+
+
+class ManifestXCanvas(models.Model):
+    manifest = models.TextField()
+    canvas = models.TextField(blank=True, null=True)
+    digitalresource = models.UUIDField()
+
+    class Meta:
+        managed = True
+        db_table = "manifest_x_canvas"
