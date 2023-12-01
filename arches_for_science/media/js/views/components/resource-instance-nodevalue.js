0daf44173bc5cf0a0ff48d2bf8711a047a8fcf36
Alexei Peters <apeters@fargeo.com>
2023-12-01 13:13:58 -0800

    fix sampling info step with selectwoo integration, re #1454

--- arches_for_science/media/js/views/components/resource-instance-nodevalue.js
+++ arches_for_science/media/js/views/components/resource-instance-nodevalue.js
@@ -23,24 +23,24 @@ define([
             });
 
             this.select2Config = {
-                value: self.renderContext === 'search' ? self.value : self.resourceToAdd,
+                value: self.onlyManageResourceIds ? self.value : self.resourceToAdd,
                 clickBubble: true,
                 disabled: self.disabled,
                 multiple: !self.displayOntologyTable ? self.multiple : false,
                 placeholder: this.placeholder() || arches.translations.riSelectPlaceholder,
                 closeOnSelect: true,
-                allowClear: self.renderContext === 'search' ? true : false,
+                allowClear: self.onlyManageResourceIds ? true : false,
                 onSelect: function(item) {
                     self.selectedItem(item);
-                    if (self.renderContext !== 'search') {
-                        var ret = self.makeObject(item.resourceinstanceid, item);
+                    if (!self.onlyManageResourceIds){
+                        var ret = self.makeObject(item.id, item);
                         self.setValue(ret);
                         window.setTimeout(function() {
                             if(self.displayOntologyTable){
-                                self.resourceToAdd("");
+                                self.clearDropDown();
                             }
-                        }, 250);
-                    }    
+                        }, 250);    
+                    } 
                 },
                 ajax: {
                     url: arches.urls.related_resources + self.relatedResourceId + "?paginate=false",
@@ -67,7 +67,8 @@ define([
                     }
                 },
                 initSelection: function(ele, callback) {
-                    if(self.renderContext === "search" && self.value() !== "") {
+                    self.select2ele = ele;
+                    if(!self.displayOntologyTable && !!self.value() && !self.graphIds().includes(self.value())) {
                         var values = self.value();
                         if(!Array.isArray(self.value())){
                             values = [self.value()];
@@ -76,25 +77,32 @@ define([
                         var lookups = [];
         
                         values.forEach(function(val){
-                            var resourceId;
-                            if (typeof val === 'string') {
-                                resourceId = val;
+                            if(!!val){
+                                var resourceId;
+                                if (typeof val === 'string') {
+                                    resourceId = val;
+                                }
+                                else if (ko.unwrap(val.resourceId)) {
+                                    resourceId = ko.unwrap(val.resourceId);
+                                }
+            
+                                var resourceInstance = ResourceUtils.lookupResourceInstanceData(resourceId).then(
+                                    function(resourceInstance) { return resourceInstance; }
+                                );
+                    
+                                if (resourceInstance) { lookups.push(resourceInstance); }
                             }
-                            else if (ko.unwrap(val.resourceId)) {
-                                resourceId = ko.unwrap(val.resourceId);
-                            }
-        
-                            var resourceInstance = ResourceUtils.lookupResourceInstanceData(resourceId).then(
-                                function(resourceInstance) { return resourceInstance; }
-                            );
-                
-                            if (resourceInstance) { lookups.push(resourceInstance); }
                         });
         
                         Promise.all(lookups).then(function(arr){
                             if (arr.length) {
                                 let ret = arr.map(function(item) {
-                                    return {"displayname": item["_source"].displayname, "resourceinstanceid": item["_id"]};
+                                    return {
+                                        "text": item["_source"].displayname, 
+                                        "displayname": item["_source"].displayname, 
+                                        "id": item["_id"],
+                                        "resourceinstanceid": item["_id"]
+                                    };
                                 });
                                 if(self.multiple === false) {
                                     ret = ret[0];
