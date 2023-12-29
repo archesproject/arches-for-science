3d47831bab98fac28c12e0c9b53af021c22172a2
Jacob Walls <jwalls@fargeo.com>
2023-12-29 15:39:46 -0500

    Traverse label-based graph through node ids #1503

--- arches_for_science/media/js/utils/annotation-utils.js
+++ arches_for_science/media/js/utils/annotation-utils.js
@@ -6,39 +6,45 @@ define(['utils/label-based-graph-utils'], function(labelBasedGraphUtils) {
             annotationCollection.parentDisplayName = resource.displayname;
             annotationCollection.parentResourceId = resource.resourceinstanceid;
             annotationCollection.canvases = {};
-            
+
+            const partIdentifierAssignmentNodes = Object.freeze({
+                physicalPart: "b240c366-8594-11ea-97eb-acde48001122",
+                label: "3e541cc6-859b-11ea-97eb-acde48001122",
+                annotator: "a623eaf4-8599-11ea-97eb-acde48001122",
+                polygonIdentifier: "97c30c42-8594-11ea-97eb-acde48001122",
+            });
+
             resource.resource["Part Identifier Assignment"].forEach(function(annotation) {
-                const annotationResourceId = labelBasedGraphUtils.getNodeValue(annotation,
-                    "Part Identifier Assignment_Physical Part of Object",
+                const annotationResourceId = labelBasedGraphUtils.getPropByNodeId(annotation,
+                    partIdentifierAssignmentNodes.physicalPart,
                     "resourceId",
                 );
-                const annotationName = labelBasedGraphUtils.getNodeValue(annotation, 
-                    "Part Identifier Assignment_Physical Part of Object",
+                const annotationName = labelBasedGraphUtils.getPropByNodeId(annotation, 
+                    partIdentifierAssignmentNodes.physicalPart,
                     "@display_value",
                 );
-                const annotationLabel = labelBasedGraphUtils.getNodeValue(annotation, 
-                    "Part Identifier Assignment_Label",
+                const annotationLabel = labelBasedGraphUtils.getPropByNodeId(annotation, 
+                    partIdentifierAssignmentNodes.label,
                     "@display_value",
                 );
-                const annotator = labelBasedGraphUtils.getNodeValue(annotation, 
-                    "Part Identifier Assignment_Annotator",
+                const annotator = labelBasedGraphUtils.getPropByNodeId(annotation, 
+                    partIdentifierAssignmentNodes.annotator,
                     "@display_value",
                 );
-                const annotationStr = labelBasedGraphUtils.getNodeValue(annotation, 
-                    "Part Identifier Assignment_Polygon Identifier",
+                const annotationStr = labelBasedGraphUtils.getPropByNodeId(annotation, 
+                    partIdentifierAssignmentNodes.polygonIdentifier,
                     "@display_value",
                 );
-                const tileId = labelBasedGraphUtils.getNodeValue(annotation, 
-                    "Part Identifier Assignment_Polygon Identifier",
+                const tileId = labelBasedGraphUtils.getPropByNodeId(annotation, 
+                    partIdentifierAssignmentNodes.polygonIdentifier,
                     "@tile_id",
                 );
                 if (annotationStr) {
                     const annotationJson = JSON.parse(annotationStr.replaceAll("'", '"'));
                     if (annotationJson.features.length > 0) {
-                        const currentManifestUrl =
-              annotation["Part Identifier Assignment_Polygon Identifier"][
-                  "geojson"
-              ]["features"][0]["properties"]["manifest"];
+                        const currentManifestUrl = labelBasedGraphUtils.getPropByNodeId(
+                            annotation, partIdentifierAssignmentNodes.polygonIdentifier, "geojson"
+                            )["features"][0]["properties"]["manifest"];
                         if (!displayedManifestUrl || currentManifestUrl === displayedManifestUrl) {
                             const canvas = annotationJson.features[0].properties.canvas;
 
