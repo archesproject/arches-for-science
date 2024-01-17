jacobtylerwalls/arches-for-science-prj

Mark strings for translation in /plugins #1503

arches_for_science/media/js/views/components/plugins/chemical-analysis-workflow.js
@@ -17,7 +17,7 @@ define([
 
             this.stepConfig = [
                 {
-                    title: 'Project Info',
+                    title: arches.translations.projectinfo,
                     name: 'project-info',
                     required: true,
                     layoutSections: [
@@ -39,7 +39,7 @@ define([
                     ],
                 },
                 {
-                    title: 'Observation Info',
+                    title: arches.translations.observationinfo,
                     name: 'select-instrument-and-files',
                     required: true,
                     lockableExternalSteps: ['project-info'],
@@ -58,9 +58,10 @@ define([
                     ],
                 },
                 {
-                    title: 'Upload Files',
+                    title: arches.translations.uploadFile,
                     name: 'upload-files',
-                    description: 'The date that the sample was taken',
+                    // TODO(jtw): seems wrong
+                    description: arches.translations.dateSampleTaken,
                     component: 'views/components/workflows/component-based-step',
                     componentname: 'component-based-step',
                     workflowstepclass: 'upload-dataset-step-workflow-component-based-step',
@@ -86,9 +87,9 @@ define([
                     ],
                 },
                 {
-                    title: 'Summary',
+                    title: arches.translations.summary,
                     name: 'chemical-analysis-complete',  /* unique to workflow */
-                    description: 'Summary',
+                    description: arches.translations.summary,
                     layoutSections: [
                         {
                             componentConfigs: [
@@ -125,11 +126,11 @@ define([
                 this.alert(
                     new AlertViewModel(
                         'ep-alert-red',
-                        'Are you sure you would like to delete this workflow?',
-                        'All data created during the course of this workflow will be deleted.',
+                        arches.translations.deleteWorkflowTitle,
+                        arches.translations.deleteWorkflowWarning,
                         function(){}, //does nothing when canceled
                         () => {
-                            params.loading('Cleaning up...');
+                            params.loading(arches.translations.cleaningUp);
                             this.reverseWorkflowTransactions();
                         },
                     )

archesproject/arches-for-science

arches_for_science/media/js/views/components/plugins/chemical-analysis-workflow.js
@@ -17,7 +17,7 @@ define([
 
             this.stepConfig = [
                 {
-                    title: 'Project Info',
+                    title: arches.translations.projectInfo,
                     name: 'project-info',
                     required: true,
                     layoutSections: [
@@ -39,7 +39,7 @@ define([
                     ],
                 },
                 {
-                    title: 'Observation Info',
+                    title: arches.translations.observationInfo,
                     name: 'select-instrument-and-files',
                     required: true,
                     lockableExternalSteps: ['project-info'],
@@ -58,9 +58,10 @@ define([
                     ],
                 },
                 {
-                    title: 'Upload Files',
+                    title: arches.translations.uploadFiles,
                     name: 'upload-files',
-                    description: 'The date that the sample was taken',
+                    // TODO(jtw): seems wrong
+                    description: arches.translations.dateSampleTaken,
                     component: 'views/components/workflows/component-based-step',
                     componentname: 'component-based-step',
                     workflowstepclass: 'upload-dataset-step-workflow-component-based-step',
@@ -86,9 +87,9 @@ define([
                     ],
                 },
                 {
-                    title: 'Summary',
+                    title: arches.translations.summary,
                     name: 'chemical-analysis-complete',  /* unique to workflow */
-                    description: 'Summary',
+                    description: arches.translations.summary,
                     layoutSections: [
                         {
                             componentConfigs: [
@@ -125,11 +126,11 @@ define([
                 this.alert(
                     new AlertViewModel(
                         'ep-alert-red',
-                        'Are you sure you would like to delete this workflow?',
-                        'All data created during the course of this workflow will be deleted.',
+                        arches.translations.deleteWorkflowTitle,
+                        arches.translations.deleteWorkflowWarning,
                         function(){}, //does nothing when canceled
                         () => {
-                            params.loading('Cleaning up...');
+                            params.loading(arches.translations.cleaningUp);
                             this.reverseWorkflowTransactions();
                         },
                     )