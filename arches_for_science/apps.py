e139ee4a4a4f07d12905ee426cf063c86030a54e
Christopher Byrd <cbyrd@fargeo.com>
2024-07-16 15:25:11 -0700

    adds apps.py

---
+++ arches_for_science/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ArchesForScienceConfig(AppConfig):
+    name = "arches_for_science"
+    verbose_name = "Arches_for_science"
+    is_arches_application = True
