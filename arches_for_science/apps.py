eb286530b33bb80d09b18e2d8d29bce8c7fa2e15
Cyrus Hiatt <chiatt@fargeo.com>
2024-07-17 08:59:37 -0700

    Update arches_for_science/apps.py
    Co-authored-by: Jacob Walls <jwalls@fargeo.com>

--- arches_for_science/apps.py
+++ arches_for_science/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class ArchesForScienceConfig(AppConfig):
     name = "arches_for_science"
-    verbose_name = "Arches_for_science"
+    verbose_name = "Arches for Science"
     is_arches_application = True
