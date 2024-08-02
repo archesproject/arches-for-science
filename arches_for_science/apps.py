072a811abb99f01123b6c324a2dd0b77674f7ba1
Cyrus Hiatt <chiatt@fargeo.com>
2024-08-02 09:00:27 -0700

    Update apps.py and gh actions for 7.6 latest

--- arches_for_science/apps.py
+++ arches_for_science/apps.py
@@ -1,7 +1,12 @@
 from django.apps import AppConfig
 
+from arches.settings_utils import generate_frontend_configuration
+
 
 class ArchesForScienceConfig(AppConfig):
     name = "arches_for_science"
     verbose_name = "Arches for Science"
     is_arches_application = True
+
+    def ready(self):
+        generate_frontend_configuration()
