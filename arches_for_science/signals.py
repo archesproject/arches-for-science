e0c8115fd623251ea1c162255fabed4c86eefa72
Cyrus Hiatt <chiatt@fargeo.com>
2024-12-03 11:44:49 -0800

    Delete part tile after relationship is deleted

--- arches_for_science/signals.py
+++ arches_for_science/signals.py
@@ -19,3 +19,12 @@ def delete_manifest_x_canvas(sender, instance, **kwargs):
     ManifestXCanvas.objects.filter(manifest=instance.manifest["@id"]).delete()
     ManifestXDigitalResource.objects.filter(manifest=instance.manifest["@id"]).delete()
 
+
+@receiver(post_delete, sender=ResourceXResource)
+def ensure_part_tile_is_deleted(sender, instance, **kwargs):
+    physical_thing_parts_node_id = uuid.UUID("b240c366-8594-11ea-97eb-acde48001122")
+    if instance.nodeid_id == physical_thing_parts_node_id:
+        instance.tileid.delete()
+        resourceid = instance.tileid.resourceinstance_id
+        resource = Resource.objects.get(pk=resourceid)
+        resource.index()
