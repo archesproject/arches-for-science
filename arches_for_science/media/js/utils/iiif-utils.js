41203f9d93b55035c8039e8575802fde925629e8
njkim <nkim@fargeo.com>
2025-06-13 11:37:24 -0700

    Add a iiif utils to handle versions, #1614

---
+++ arches_for_science/media/js/utils/iiif-utils.js
@@ -0,0 +1,127 @@
+define([], function() {
+    const getVersion = (manifestData) => {
+        if (manifestData?.['@context'] === 'http://iiif.io/api/presentation/3/context.json') {
+            return 3;
+        } else if (manifestData?.['@context'] === 'http://iiif.io/api/presentation/2/context.json') {
+            return 2;
+        }
+    };
+    const getManifestDataValue = function(object, property, returnFirstVal, version, i18n) {
+        let val = object[property];
+        if (version === 2) {
+            if (Array.isArray(val) && returnFirstVal) val = val[0]
+        } else if (version === 3) {
+            if (i18n) val = val[i18n]
+            if (Array.isArray(val) && returnFirstVal) val = val[0]
+        }
+        return val;
+    };
+    const getCanvases = (manifestData) => {
+        const canvases = [];
+        if (getVersion(manifestData) === 3) {
+            const sequences = manifestData ? manifestData.items : [];
+            sequences.forEach(function(canvas_item) {
+                canvas = { items: canvas_item.items }
+                canvas.label = canvas_item.label["en"][0];
+                canvas.id = getCanvasService(canvas_item, 3);
+                canvas.text = canvas.label;
+                if (typeof canvas_item.thumbnail === 'object')
+                    canvas.thumbnail = canvas_item.thumbnail[0]["id"];
+                else if (canvas_item.items?.[0]?.items?.[0]?.body?.id)
+                    canvas.thumbnail = canvas_item.items?.[0]?.items?.[0]?.body?.id;
+                canvases.push(canvas);
+            })
+        } else if (getVersion(manifestData) === 2) {
+            const sequences = manifestData ? manifestData.sequences : [];
+            sequences.forEach(function(sequence) {
+                if (sequence.canvases) {
+                    sequence.label = getManifestDataValue(sequence, 'label', true);
+                    sequence.canvases.forEach(function(canvas) {
+                        canvas.label = getManifestDataValue(canvas, 'label', true);
+                        if (typeof canvas.thumbnail === 'object')
+                            canvas.thumbnail = canvas.thumbnail["@id"];
+                        else if (canvas.images && canvas.images[0] && canvas.images[0].resource)
+                            canvas.thumbnail = canvas.images[0].resource["@id"];
+                        canvas.id = getCanvasService(canvas, 2);
+                        canvas.text = canvas.label;
+                        canvases.push(canvas);
+                    });
+                }
+            })
+        };
+        return canvases;
+    };
+    const getCanvas = (manifestData, canvasId, updateCanvas) => {
+        if (getVersion(manifestData) === 3) {
+            if (manifestData.items.length > 0) {
+                canvases = manifestData.items;
+                let canvasIndex = 0;
+                if (!updateCanvas) {
+                    console.log("canvases", canvases)
+                    canvasIndex = canvases.findIndex((c) => (c.items[0].id === canvasId));
+                }
+                return canvases[canvasIndex];
+            }
+        } else if (getVersion(manifestData) === 2) {
+            if (manifestData.sequences.length > 0) {
+                const sequence = manifestData.sequences[0];
+                let canvasIndex = 0;
+                if (sequence.canvases.length > 0) {
+                    if (!updateCanvas) {
+                        canvasIndex = sequence.canvases.findIndex((c) => (c.images[0].resource.service['@id'] === canvasId));
+                    }
+                    return sequence.canvases[canvasIndex];
+                }    
+            }
+        }
+        return null;
+    };
+    const getCanvasLabel = (canvas, version) => {
+        if (version === 2) {
+            return canvas.label;
+        } else if (version === 3) {
+            return canvas.label?.["en"]?.[0];
+        }
+    };
+    const getCanvasService = (canvas, version) => {
+        if (version === 2) {
+            return canvas.images?.[0]?.resource.service['@id'];
+        } else if (version === 3) {
+            return canvas.id.replace("/canvas", "");
+        };
+    };
+    const getMetadata = (manifestData) => {
+        if (getVersion(manifestData) === 2) {
+            return manifestData.metadata;
+        } else if (getVersion(manifestData) === 3) {
+            return manifestData.metadata.map((data) => {
+                value = {};
+                Object.entries(data).forEach(([k, v]) => {
+                    value[k] = v['en'][0];
+                });
+                return value;
+            });
+        };
+    };
+    // I need a better name for this function
+    const changeCanvas = (manifestData) => {
+        if (getVersion(manifestData) === 2) {
+            return manifestData.sequences[0].canvases[0];
+        } else if (getVersion(manifestData) === 3) {
+            return manifestData.items[0];
+        }
+    };
+
+
+
+    return {
+        getVersion,
+        getManifestDataValue,
+        getCanvases,
+        getCanvas,
+        getCanvasLabel,
+        getCanvasService,
+        getMetadata,
+        changeCanvas,
+    };
+});
