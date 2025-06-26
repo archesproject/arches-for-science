70dfc5b9b5adfb2d66edbbeb8e6abc6314ca3efb
njkim <nkim@fargeo.com>
2025-06-25 22:07:19 -0700

    Create manifest record from the external service, #1626

--- arches_for_science/views/manifest_x_canvas.py
+++ arches_for_science/views/manifest_x_canvas.py
@@ -1,10 +1,13 @@
 from http import HTTPStatus
+import uuid
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import View
 
 from arches.app.utils.response import JSONErrorResponse, JSONResponse
+from arches.app.models.models import IIIFManifest
 from arches_for_science.models import ManifestXDigitalResource, CanvasXDigitalResource
+from arches_for_science.utils.digital_resource_for_manifest import create_manifest_record, create_digital_resource_from_manifest
 
 
 class ManifestXCanvasView(View):
@@ -21,7 +24,10 @@ class ManifestXCanvasView(View):
                 canvas = link.canvas
             digital_resource = link.digitalresource
         elif manifest:
+            if not IIIFManifest.objects.filter(url=manifest).exists():
+                create_manifest_record(manifest)
             digital_resource = ManifestXDigitalResource.objects.get(manifest=manifest).digitalresource
+
         elif canvas:
             digital_resource = CanvasXDigitalResource.objects.get(canvas=canvas).digitalresource
         else:
