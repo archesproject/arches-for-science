c62fcdba907b7e3c4091458401f4626b2e8cb147
njkim <nkim@fargeo.com>
2025-06-26 15:59:01 -0700

    Get correct correct service url, #1632

--- arches_for_science/utils/thumbnail_fetchers/digital_resource_thumbnail_fetcher.py
+++ arches_for_science/utils/thumbnail_fetchers/digital_resource_thumbnail_fetcher.py
@@ -11,12 +11,21 @@ class DigitalResourceThumbnailFetcher(SearchThumbnailFetcher):
         IIIF_MANIFEST_TYPE = "305c62f0-7e3d-4d52-a210-b451491e6100"
         MANIFEST_NODEGROUP_ID = "56f8e26e-ca7c-11e9-9aa3-a4d18cec433a"
         MANIFEST_URL_NODE_ID = "56f8e9bd-ca7c-11e9-b578-a4d18cec433a"
+        ID_TYPE_NODE_ID = "56f8e759-ca7c-11e9-bda1-a4d18cec433a"
+        URL_VALUE_ID = "f32d0944-4229-4792-a33c-aadc2b181dc7"
         self.resource.load_tiles()
         digital_resource_type_tile = next(
             (tile for tile in self.resource.tiles if str(tile.nodegroup_id) == DIGITAL_RESOURCE_TYPE_NODEGROUP_ID), None
         )
         if digital_resource_type_tile and IIIF_MANIFEST_TYPE in digital_resource_type_tile.data[DIGITAL_RESOURCE_TYPE_NODEGROUP_ID]:
-            manifest_tile = next((tile for tile in self.resource.tiles if str(tile.nodegroup_id) == MANIFEST_NODEGROUP_ID), None)
+            manifest_tile = next(
+                (
+                    tile
+                    for tile in self.resource.tiles
+                    if (str(tile.nodegroup_id) == MANIFEST_NODEGROUP_ID and URL_VALUE_ID in tile.data[ID_TYPE_NODE_ID])
+                ),
+                None,
+            )
             manifest_url = manifest_tile.data[MANIFEST_URL_NODE_ID][get_language()]["value"]
             response = requests.get(manifest_url)
             try:
