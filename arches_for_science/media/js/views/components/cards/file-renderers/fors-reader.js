1e21697dd029da54be9cebe376289870179d66fd
njkim <nkim@fargeo.com>
2026-01-30 15:23:13 -0800

    Remove require js

--- arches_for_science/media/js/views/components/cards/file-renderers/fors-reader.js
+++ arches_for_science/media/js/views/components/cards/file-renderers/fors-reader.js
@@ -1,58 +1,56 @@
-define(['jquery',
-    'arches',
-    'knockout',
-    'templates/views/components/cards/file-renderers/afs-reader.htm',
-    'viewmodels/afs-instrument',
-    'bindings/plotly',
-    'bindings/select2-query',
-], function($, arches, ko, forsReaderTemplate, AfsInstrumentViewModel) {
-    return ko.components.register('fors-reader', {
-        viewModel: function(params) {
-            const self = this;
-            AfsInstrumentViewModel.apply(this, [params]);
+import arches from 'arches';
+import ko from 'knockout';
+import forsReaderTemplate from 'templates/views/components/cards/file-renderers/afs-reader.htm';
+import AfsInstrumentViewModel from 'viewmodels/afs-instrument';
+import 'bindings/plotly';
+import 'bindings/select2-query';
 
-            this.parse = function(data, series){
-                const vals = data.split('\n');
-                let metadata = {};
-                vals.forEach(function(val) {
-                    if (val.startsWith('##')) {
-                        if (val.startsWith('##YFACTOR')) {
-                            metadata.yfactor = Number(val.split('=')[1]);
-                        }
-                        if (val.startsWith('##XUNITS')) {
-                            metadata.xunits = val.split('=')[1];
-                        }
-                        if (val.startsWith('##YUNITS')) {
-                            const yunits = val.split('=')[1];
-                            metadata.yunits = yunits ? `${yunits[0].toUpperCase()}${yunits.slice(1).toLowerCase()}` : '';
-                        }
+export default ko.components.register('fors-reader', {
+    viewModel: function(params) {
+        const self = this;
+        AfsInstrumentViewModel.apply(this, [params]);
+
+        this.parse = function(data, series){
+            const vals = data.split('\n');
+            let metadata = {};
+            vals.forEach(function(val) {
+                if (val.startsWith('##')) {
+                    if (val.startsWith('##YFACTOR')) {
+                        metadata.yfactor = Number(val.split('=')[1]);
                     }
-                    if (val.startsWith('Spectrum file is ')) {
-                        const yunits = val.split(' ')[3];
+                    if (val.startsWith('##XUNITS')) {
+                        metadata.xunits = val.split('=')[1];
+                    }
+                    if (val.startsWith('##YUNITS')) {
+                        const yunits = val.split('=')[1];
                         metadata.yunits = yunits ? `${yunits[0].toUpperCase()}${yunits.slice(1).toLowerCase()}` : '';
                     }
-                    let rec;
-                    if (val.includes('\t')) {
-                        rec = val.trim().split('\t');
-                        if (Number(rec[0] >= 350 && rec[0] <= 2500)) {
-                            series.value.push(Number(rec[0]));
-                            series.count.push(Number(rec[1]));
-                        }
-                    } else {
-                        rec = val.trim().split(/(?=[+-])/g);
-                        if (Number(rec[0] >= 350 && rec[0] <= 2500)) {
-                            series.value.push(Number(rec[0]));
-                            rec.splice(0, 1);
-                            const average = rec.reduce((a,b) => Number(a) + Number(b), 0) / rec.length;
-                            series.count.push(average*metadata.yfactor);
-                        }
+                }
+                if (val.startsWith('Spectrum file is ')) {
+                    const yunits = val.split(' ')[3];
+                    metadata.yunits = yunits ? `${yunits[0].toUpperCase()}${yunits.slice(1).toLowerCase()}` : '';
+                }
+                let rec;
+                if (val.includes('\t')) {
+                    rec = val.trim().split('\t');
+                    if (Number(rec[0] >= 350 && rec[0] <= 2500)) {
+                        series.value.push(Number(rec[0]));
+                        series.count.push(Number(rec[1]));
                     }
-                });
-                self.chartTitle(arches.translations.asdSpectrum);
-                self.xAxisLabel(arches.translations.wavelengthLabel);
-                self.yAxisLabel(metadata.yunits);
-            };
-        },
-        template: forsReaderTemplate
-    });
+                } else {
+                    rec = val.trim().split(/(?=[+-])/g);
+                    if (Number(rec[0] >= 350 && rec[0] <= 2500)) {
+                        series.value.push(Number(rec[0]));
+                        rec.splice(0, 1);
+                        const average = rec.reduce((a,b) => Number(a) + Number(b), 0) / rec.length;
+                        series.count.push(average*metadata.yfactor);
+                    }
+                }
+            });
+            self.chartTitle(arches.translations.asdSpectrum);
+            self.xAxisLabel(arches.translations.wavelengthLabel);
+            self.yAxisLabel(metadata.yunits);
+        };
+    },
+    template: forsReaderTemplate
 });
--- arches_for_science/media/js/views/components/workflows/review-dataset/select-dataset.js
+++ arches_for_science/media/js/views/components/workflows/review-dataset/select-dataset.js
@@ -1,77 +1,75 @@
-define([
-    'jquery',
-    'knockout',
-    'arches',
-    'templates/views/components/workflows/review-dataset/select-dataset.htm'
-], function($, ko, arches,selectDatasetStepTemplate) {
-    function viewModel(params) {
-        var self = this;
-        this.digitalResourceGraphId = '707cbd78-ca7a-11e9-990b-a4d18cec433a';
-        this.physicalThingResourceId = ko.observable();
-        this.relatedDigitalResources = ko.observableArray();
+import $ from 'jquery';
+import ko from 'knockout';
+import arches from 'arches';
+import selectDatasetStepTemplate from 'templates/views/components/workflows/review-dataset/select-dataset.htm';
 
-        this.dataLoaded = ko.observable(false);
+function viewModel(params) {
+    var self = this;
+    this.digitalResourceGraphId = '707cbd78-ca7a-11e9-990b-a4d18cec433a';
+    this.physicalThingResourceId = ko.observable();
+    this.relatedDigitalResources = ko.observableArray();
 
-        const getDigitalResources = async function(resourceid) {
-            if(!resourceid){ return; }
-            const url = `${arches.urls.root}digital-resources-by-object-parts/${resourceid}`;
-            const result = await fetch(url, {
-                method: 'GET',
-                credentials: 'include'
-            });
-            if (result.ok) {
-                const results = await result.json();
-                const resources = results.resources;
+    this.dataLoaded = ko.observable(false);
 
-                resources.forEach(function(resource) {
-                    resource.selected = ko.observable(false);
-                    if(params.value()) {
-                        params.value().digitalResources.forEach(function(val) {
-                            if (val.resourceid === resource.resourceid) {
-                                resource.selected(val.selected);
-                            }
-                        });
-                    }
-                });
-                const digitalResourcesBelongToParentOrSample = resources.filter(resource => {
-                    return (!resource.isdirect && results.type !== 'sample') ||
-                        (resource.isdirect && results.type === 'sample');
-                });
-                self.relatedDigitalResources(digitalResourcesBelongToParentOrSample);
-                self.dataLoaded(true);
-            }
-        };
-
-        this.selectedDigtalResources = ko.pureComputed(function() {
-            return self.relatedDigitalResources().map(function(resource){
-                return {
-                    resourceid: resource.resourceid,
-                    partresourceid: resource.partresourceid,
-                    selected: resource.selected()
-                };
-            });
+    const getDigitalResources = async function(resourceid) {
+        if(!resourceid){ return; }
+        const url = `${arches.urls.root}digital-resources-by-object-parts/${resourceid}`;
+        const result = await fetch(url, {
+            method: 'GET',
+            credentials: 'include'
         });
+        if (result.ok) {
+            const results = await result.json();
+            const resources = results.resources;
 
-        this.selectedDigtalResources.subscribe(function(val) {
-            if(val && val.length > 0){
-                const data = {digitalResources: val, resourceid: self.physicalThingResourceId()};
-                params.value(data);
-            } else {
-                params.value(undefined);
-            }
-        });
-
-        this.physicalThingResourceId.subscribe(getDigitalResources);
-
-        if (params.value()) {
-            self.physicalThingResourceId(params.value().resourceid);
-            getDigitalResources(params.value().resourceid);
+            resources.forEach(function(resource) {
+                resource.selected = ko.observable(false);
+                if(params.value()) {
+                    params.value().digitalResources.forEach(function(val) {
+                        if (val.resourceid === resource.resourceid) {
+                            resource.selected(val.selected);
+                        }
+                    });
+                }
+            });
+            const digitalResourcesBelongToParentOrSample = resources.filter(resource => {
+                return (!resource.isdirect && results.type !== 'sample') ||
+                    (resource.isdirect && results.type === 'sample');
+            });
+            self.relatedDigitalResources(digitalResourcesBelongToParentOrSample);
+            self.dataLoaded(true);
         }
-    }
+    };
 
-    ko.components.register('select-dataset', {
-        viewModel: viewModel,
-        template: selectDatasetStepTemplate
+    this.selectedDigtalResources = ko.pureComputed(function() {
+        return self.relatedDigitalResources().map(function(resource){
+            return {
+                resourceid: resource.resourceid,
+                partresourceid: resource.partresourceid,
+                selected: resource.selected()
+            };
+        });
     });
-    return viewModel;
+
+    this.selectedDigtalResources.subscribe(function(val) {
+        if(val && val.length > 0){
+            const data = {digitalResources: val, resourceid: self.physicalThingResourceId()};
+            params.value(data);
+        } else {
+            params.value(undefined);
+        }
+    });
+
+    this.physicalThingResourceId.subscribe(getDigitalResources);
+
+    if (params.value()) {
+        self.physicalThingResourceId(params.value().resourceid);
+        getDigitalResources(params.value().resourceid);
+    }
+}
+
+ko.components.register('select-dataset', {
+    viewModel: viewModel,
+    template: selectDatasetStepTemplate
 });
+export default viewModel;
