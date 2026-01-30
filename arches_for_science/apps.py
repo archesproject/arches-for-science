6334e2ef2cd761eb75403ed8b91165d1cfb5be61
njkim <nkim@fargeo.com>
2026-01-29 17:30:32 -0800

    Follow upgrade instructions

--- arches_for_science/apps.py
+++ arches_for_science/apps.py
@@ -1,13 +1,7 @@
 from django.apps import AppConfig
 
-from arches.settings_utils import generate_frontend_configuration
-
 
 class ArchesForScienceConfig(AppConfig):
     name = "arches_for_science"
     verbose_name = "Arches for Science"
     is_arches_application = True
-
-    def ready(self):
-        generate_frontend_configuration()
-        from . import signals
